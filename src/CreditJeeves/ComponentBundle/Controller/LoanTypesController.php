4cfc79a68327cc23917b6ccc7af944db572f4fb3
alex <alex.emelyanov.ua@gmail.com>
2013-03-15 11:53:44 +0200

    In progress Loan Types component

---
+++ src/CreditJeeves/ComponentBundle/Controller/LoanTypesController.php
@@ -0,0 +1,39 @@
+<?php
+namespace CreditJeeves\ComponentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CreditJeeves\CoreBundle\Arf\ArfParser;
+
+class LoanTypesController extends Controller
+{
+    /**
+     *
+     * @var integer
+     */
+    const MAX_DIAL = 12;
+    
+    public function indexAction()
+    {
+        $ArfReport = $this->getUser()->getReportsPrequal()->last()->getArfReport();
+        $RevolvingDept   = $ArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_BALANCE_TOTAL_REVOLVING
+        );
+        $MortgageDebt    = $ArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_BALANCE_REAL_ESTATE
+        );
+        $InstallmentDebt = $ArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_BALANCE_INSTALLMENT
+        );
+        return $this->render(
+            'ComponentBundle:LoanTypes:index.html.twig',
+            array(
+                'RevolvingDept' => $RevolvingDept,
+                'MortgageDebt' => $MortgageDebt,
+                'InstallmentDebt' => $InstallmentDebt,
+                )
+            );
+    }
+}
