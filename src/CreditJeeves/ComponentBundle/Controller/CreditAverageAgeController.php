6735a213757d7ce66b73ec22c60adda3e3521a9d
alex <alex.emelyanov.ua@gmail.com>
2013-03-15 13:04:22 +0200

    In progress Credit Average Age component

---
+++ src/CreditJeeves/ComponentBundle/Controller/CreditAverageAgeController.php
@@ -0,0 +1,37 @@
+<?php
+namespace CreditJeeves\ComponentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CreditAverageAgeController extends Controller
+{
+    public function indexAction()
+    {
+        $nOldest = 0;
+        $nAge = 0;
+        $nTotal = 0;
+        $aTradelines = $this->getUser()->getReportsPrequal()->last()->getTradeLines();
+        $oCurrentDate = new \DateTime('now');
+        foreach ($aTradelines as $aTradeline) {
+          $nTotal++;
+          $oOpenedDate = \DateTime::createFromFormat('my', $aTradeline['date_open']);
+          $interval = $oOpenedDate->diff($oCurrentDate);
+          $nMonthes = $interval->format('%y') * 12 + $interval->format('%m');
+          $nAge += $nMonthes;
+          if ($nMonthes > $nOldest) {
+            $nOldest = $nMonthes;
+          }
+        }
+        if ($nTotal > 0) {
+            $nAge = floor($nAge / ($nTotal * 12));
+            $nOldest = floor($nOldest / 12);
+        }
+        return $this->render(
+            'ComponentBundle:CreditAverageAge:index.html.twig',
+            array(
+                'nOldest' => $nOldest,
+                'nAge' => $nAge,
+                )
+            );
+    }
+}
