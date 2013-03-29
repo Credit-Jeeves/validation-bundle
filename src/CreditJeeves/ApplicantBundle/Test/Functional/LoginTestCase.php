92b9467c69914ba7678e323bc817149549ec9977
Ton Sharp <66ton99@gmail.com>
2013-03-29 20:32:11 +0200

    Setup base test env

---
+++ src/CreditJeeves/ApplicantBundle/Test/Functional/LoginTestCase.php
@@ -0,0 +1,15 @@
+<?php
+namespace CreditJeeves\ApplicantBundle\Test\Functional;
+
+use CreditJeeves\CoreBundle\Test\Functional\BaseTestCase;
+
+class LoginTestCase extends BaseTestCase
+{
+    /**
+     * @test
+     */
+    public function userCanLogin()
+    {
+        $this->get
+    }
+}
---
+++ src/CreditJeeves/CoreBundle/Test/Functional/BaseTestCase.php
@@ -0,0 +1,9 @@
+<?php
+namespace CreditJeeves\CoreBundle\Test\Functional;
+
+use Behat\MinkBundle\Test\MinkTestCase;
+
+abstract class BaseTestCase extends MinkTestCase
+{
+
+}
