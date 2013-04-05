3f8fd18f73fb40782b6db29d283c15f3b43b31a6
Ton Sharp <66ton99@gmail.com>
2013-04-05 22:39:33 +0300

    Integrated CJ1 in to the CJ2 project

--- bin/environment.php
+++ bin/environment.php
@@ -1,10 +1,13 @@
 <?php
 
+use Hat\Environment\Environment;
+use Hat\Environment\Kit\Kit;
+
 $libRoot = realpath(__DIR__ . '/../vendor/hat/environment') . '/';
 require_once $libRoot . 'autoload.php';
 
 $configs = require $libRoot . 'src/Hat/Environment/Environment.config.php';
 $configs['default.profile.name'] = 'data/environment/profiles.ini';
 
-$environment = new Hat\Environment\Environment(new \Hat\Environment\Kit\Kit($configs));
+$environment = new Environment(new Kit($configs));
 $environment();
