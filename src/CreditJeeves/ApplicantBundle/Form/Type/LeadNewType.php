9ad8341944a12124a102e4b9b5d7f2b76049fa88
alex <alex.emelyanov.ua@gmail.com>
2013-04-16 12:48:28 +0300

    In progres Story-1395 user forms components

---
+++ src/CreditJeeves/ApplicantBundle/Form/Type/LeadNewType.php
@@ -0,0 +1,67 @@
+<?php
+namespace CreditJeeves\ApplicantBundle\Form\Type;
+
+use CreditJeeves\ApplicantBundle\Form\Type\GroupType;
+use CreditJeeves\ApplicantBundle\Form\Type\UserNewType;
+use CreditJeeves\ApplicantBundle\Form\Type\TosType;
+use CreditJeeves\ApplicantBundle\Form\Type\NewPasswordType;
+use CreditJeeves\ApplicantBundle\Form\DataTransformer\CodeToGroupTransformer;
+use CreditJeeves\ApplicantBundle\Form\DataTransformer\EmailToUserTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LeadNewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $entityManager = $options['em'];
+        $groupTransformer = new CodeToGroupTransformer($entityManager);
+        $userTransformer = new EmailToUserTransformer($entityManager);
+        $builder->add(
+            'user',
+            new UserNewType()
+        );
+        $builder->add(
+            $builder->create(
+                'code',
+                'text',
+                array(
+                    'property_path' => 'group',
+                    'label' => 'Dealer Code',
+                    )
+            )->addModelTransformer($groupTransformer)
+        );
+        $builder->add(
+            $builder->create(
+                'email',
+                'email',
+                array(
+                    'property_path' => 'user'
+                    )
+            )->addModelTransformer($userTransformer)
+        );
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'CreditJeeves\DataBundle\Entity\Lead',
+                'csrf_protection' => true,
+                'csrf_field_name' => '_token',
+                // a unique key to help generate the secret token
+                'intention' => 'username',
+            )
+        );
+
+        $resolver->setRequired(array('em'));
+
+        $resolver->setAllowedTypes(array('em' => 'Doctrine\Common\Persistence\ObjectManager'));
+    }
+
+    public function getName()
+    {
+        return 'creditjeeves_applicantbundle_leadtype';
+    }
+}
