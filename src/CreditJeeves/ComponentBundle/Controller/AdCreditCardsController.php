2f6ad81a21d5054d0fb822d31561dd9887983e97
alex <alex.emelyanov.ua@gmail.com>
2013-04-17 11:29:57 +0300

    In progress Story-1484 finished

---
+++ src/CreditJeeves/ComponentBundle/Controller/AdCreditCardsController.php
@@ -0,0 +1,161 @@
+<?php
+namespace CreditJeeves\ComponentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use CreditJeeves\DataBundle\Entity\Report;
+use CreditJeeves\DataBundle\Entity\Score;
+use CreditJeeves\CoreBundle\Arf\ArfParser;
+
+class AdCreditCardsController extends Controller
+{
+    /**
+     * @Template
+     *
+     * @return array
+     */
+    public function indexAction(Report $Report)
+    {
+        $cjArfReport = $Report->getArfReport();
+        $nInquiries = $cjArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_INQUIRIES_DURING_LAST_6_MONTHS_COUNTER
+        );
+        $sInquiries = $this->formatInquiries($nInquiries);
+        $nRevolvingDept   = $cjArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_BALANCE_TOTAL_REVOLVING
+        );
+        $isRevolving = empty($nRevolvingDept) ? "false" : "true";
+        $nMortgageDebt    = $cjArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_BALANCE_REAL_ESTATE
+        );
+        $isMortgage = empty($nMortgageDebt) ? "false" : "true";
+        $nTotal = $Report->getCountApplicantTotalTradelines();
+        $sTotal = $this->formatTradelinesCount($nTotal);
+        $nTier        = 9;
+        $aScore = $this->get('core.session.applicant')->getUser()->getScores()->last();
+        if (!empty($aScore)) {
+            $nScore = $aScore->getScore();
+            $nTier = Score::getTier($nScore);
+        }
+        $nAvailableDebt = 100 - $cjArfReport->getValue(
+            ArfParser::SEGMENT_PROFILE_SUMMARY,
+            ArfParser::REPORT_TOTAL_REVOLVING_AVAILABLE_PERCENT
+        );
+        $sUtilization = $this->formatUtilization($nAvailableDebt);
+        $aRecords = $Report->getApplicantPublicRecords();
+        $sBankruptcy = $this->formatBankruptcy($aRecords);
+        return array(
+            'isMortgage' => $isMortgage,
+            'isRevolving' => $isRevolving,
+            'sUtilization' => $sUtilization,
+            'nTier' => $nTier,
+            'sTotal' => $sTotal,
+            'sInquiries' => $sInquiries,
+            'sBankruptcy' => $sBankruptcy,
+            );
+    }
+
+    /**
+     * 
+     * @param integer $nInquiries
+     * @return string
+     */
+    private function formatInquiries($nInquiries)
+    {
+        $sInquiries = "1-5";
+        if ($nInquiries < 1) {
+            $sInquiries = "0";
+        }
+        if ($nInquiries > 5) {
+            $sInquiries = "5plus";
+        }
+        return $sInquiries;
+    }
+
+    /**
+     * 
+     * @param integer $nTotal
+     * @return string
+     */
+    private function formatTradelinesCount($nTotal)
+    {
+        $sTotal = "0";
+        if ($nTotal > 30) {
+            return "30plus";
+        }
+        if ($nTotal > 20) {
+            return "21-30";
+        }
+        if ($nTotal > 15) {
+            return "16-20";
+        }
+        if ($nTotal > 10) {
+            return "11-15";
+        }
+        if ($nTotal > 6) {
+            return "7-10";
+        }
+        if ($nTotal > 3) {
+            return "4-6";
+        }
+        if ($nTotal > 0) {
+            return "1-3";
+        }
+        return $nTotal;
+    }
+
+    /**
+     * 
+     * @param integer $nAvailableDebt
+     * @return string
+     */
+    private function formatUtilization($nAvailableDebt)
+    {
+        $sUtilization = "0-30";
+        if ($nAvailableDebt > 90) {
+            return "90-100";
+        }
+        if ($nAvailableDebt > 70) {
+            return "70-90";
+        }
+        if ($nAvailableDebt > 50) {
+            return "50-70";
+        }
+        if ($nAvailableDebt > 30) {
+            return "30-50";
+        }
+        return $sUtilization;
+    }
+
+    /**
+     * 
+     * @param array $aPublicRecords
+     * @return string
+     */
+    private function formatBankruptcy($aPublicRecords)
+    {
+        $sBankruptcy = "none";
+        $Now = new \DateTime();
+        $nMonths = 0;
+        foreach ($aPublicRecords as $Record) {
+            if (in_array($Record['code'], array(13, 23, 25, 27))) {
+                $Date = \DateTime::createFromFormat('m/d/y', $Record['status_date']);
+                $interval = $Now->diff($Date);
+                $nDiff = $interval->format('%m');
+                if ($nDiff > $nMonths) {
+                    $nMonths = $nDiff;
+                }
+            }
+        }
+        if ($nMonths > 12) {
+            return "bey12";
+        }
+        if ($nMonths > 0) {
+            return "win12";
+        }
+        return $sBankruptcy;
+    }
+}
---
+++ src/CreditJeeves/DataBundle/Model/Score.php
@@ -0,0 +1,146 @@
+<?php
+namespace CreditJeeves\DataBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\MappedSuperclass
+ */
+abstract class Score
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $cj_applicant_id;
+
+    /**
+     * @ORM\Column(type="encrypt")
+     */
+    protected $score;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_date;
+    
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="CreditJeeves\DataBundle\Entity\User",
+     *     inversedBy="scores"
+     * )
+     * @ORM\JoinColumn(
+     *     name="cj_applicant_id",
+     *     referencedColumnName="id"
+     * )
+     */
+    protected $user;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set cj_applicant_id
+     *
+     * @param integer $cjApplicantId
+     * @return Score
+     */
+    public function setCjApplicantId($cjApplicantId)
+    {
+        $this->cj_applicant_id = $cjApplicantId;
+    
+        return $this;
+    }
+
+    /**
+     * Get cj_applicant_id
+     *
+     * @return integer
+     */
+    public function getCjApplicantId()
+    {
+        return $this->cj_applicant_id;
+    }
+
+    /**
+     * Set score
+     *
+     * @param string $score
+     * @return Score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * Get score
+     *
+     * @return string
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Set created_date
+     *
+     * @param \DateTime $createdDate
+     * @return Score
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->created_date = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * Get created_date
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate()
+    {
+        return $this->created_date;
+    }
+
+    /**
+     * Set User
+     *
+     * @param User $user
+     * @return Score
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+    
+        return $this;
+    }
+
+    /**
+     * Get User
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
