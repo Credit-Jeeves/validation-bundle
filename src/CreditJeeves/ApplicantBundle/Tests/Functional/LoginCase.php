817e97b619d7b3e4b7b45f0758bd5afe65771184
alex <alex.emelyanov.ua@gmail.com>
2013-05-21 15:42:09 +0300

    In progress Story-1589 fixed code style

--- src/CreditJeeves/ApplicantBundle/Tests/Functional/LoginCase.php
+++ src/CreditJeeves/ApplicantBundle/Tests/Functional/LoginCase.php
@@ -29,6 +29,7 @@ class LoginCase extends BaseTestCase
     public function userCanLogin()
     {
         $this->load($this->fixtures, true);
+        $this->setDefaultSession('goutte');
         $this->login('emilio@example.com', 'pass');
         $this->logout();
     }
