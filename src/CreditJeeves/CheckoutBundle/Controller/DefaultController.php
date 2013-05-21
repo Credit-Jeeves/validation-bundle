ae5654a05347ee8ebfe7b0f19b00e965efe5322d
Ton Sharp <66ton99@gmail.com>
2013-05-21 23:02:58 +0300

    Buy report in progressgit push

--- src/CreditJeeves/CheckoutBundle/Controller/DefaultController.php
+++ src/CreditJeeves/CheckoutBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use \DateTime;
+use Symfony\Component\Validator\Constraints\Range;
 
 /**
  * @method \CreditJeeves\DataBundle\Entity\Applicant getUser
@@ -12,14 +13,33 @@ use \DateTime;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/checkout")
+     * @Route("/checkout", name="checkout_default")
      * @Template()
      */
     public function indexAction()
     {
-        return array(
+        $form = $this->createPurchaseForm();
 
+        return array(
+            'form' => $form->createView()
         );
+    }
+
+    /**
+     * @return \Symfony\Component\Form\Form
+     */
+    protected function createPurchaseForm()
+    {
+        return $this->createFormBuilder()
+            ->add('amount', null, array(
+                    'data' => 1.23,
+                    'constraints' => array(new Range(array('max' => 2)))
+                ))
+            ->add('card_number', null, array('data' => '4007000000027'))
+            ->add('card_expiration_date', null, array('data' => '10/16'))
+
+            ->getForm()
+            ;
     }
 
     /**
