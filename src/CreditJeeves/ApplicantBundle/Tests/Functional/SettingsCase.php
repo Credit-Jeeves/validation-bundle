1db32c2aecbdacb231a0327d9c32e4be1020bdcf
alex <alex.emelyanov.ua@gmail.com>
2013-05-22 16:11:34 +0300

    In progress Story-1589 returned test

--- src/CreditJeeves/ApplicantBundle/Tests/Functional/SettingsCase.php
+++ src/CreditJeeves/ApplicantBundle/Tests/Functional/SettingsCase.php
@@ -132,4 +132,85 @@ class SettingsCase extends BaseTestCase
         $this->login('emilio@example.com', $this->password);
         $this->logout();
     }
+
+    /**
+     * @test
+     */
+    public function userReturned()
+    {
+        $this->setDefaultSession('selenium2');
+        $this->login('emilio@example.com', $this->password);
+        $this->assertNotNull($form = $this->page->find('css', '.pod-middle form'));
+        $this->assertNotNull($submit = $form->findButton('common.get.score'));
+        $this->fillForm(
+            $form,
+            array(
+                'creditjeeves_applicantbundle_leadreturnedtype_code' => 'DVRWP2NFQ6',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_street_address1' => 'SAINT NAZAIRE 2010',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_unit_no' => '116TH 1',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_city' => 'HOMESTEAD',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_state' => 'FL',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_zip' => '33039',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_phone' => '7188491319',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_date_of_birth_day' => '19',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_date_of_birth_month' => 'Feb',
+                'creditjeeves_applicantbundle_leadreturnedtype_user_date_of_birth_year' => '1957',
+            )
+        );
+        $this->assertNotNull(
+            $ssn1 = $this->page->find(
+                'css',
+                '#ssn_creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn1'
+            )
+        );
+        $ssn1->click();
+        $this->fillForm(
+            $form,
+            array(
+                'creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn1' => '666',
+            )
+        );
+        $submit->click();
+        $this->assertNotNull(
+            $ssn2 = $this->page->find(
+                'css',
+                '#ssn_creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn2'
+            )
+        );
+        $ssn2->click();
+        $this->session->wait(
+            $this->timeout + 10000,
+            "jQuery('#ssn_creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn2').css('display') == 'none'"
+        );
+        $this->fillForm(
+            $form,
+            array(
+                 'creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn2' => '81',
+            )
+        );
+        $submit->click();
+        $this->assertNotNull(
+            $ssn3 = $this->page->find(
+                'css',
+                '#ssn_creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn3'
+            )
+        );
+        $ssn3->click();
+        $this->fillForm(
+            $form,
+            array(
+                'creditjeeves_applicantbundle_leadreturnedtype_user_ssn_ssn3' => '0987',
+            )
+        );
+        $submit->click();
+        $this->assertNotNull($check = $this->page->findAll('css', 'form .checkbox-off'));
+        $this->assertCount(1, $check, 'Wrong number of checkboxes');
+        $check[0]->click();
+        $this->session->wait(
+            $this->timeout + 10000,
+            "jQuery('form .checkbox-on').length > 0"
+        );
+        $submit->click();
+        $this->logout();
+    }
 }
