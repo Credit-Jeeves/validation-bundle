8a7bc356fca6c9c92fd190c215cf5fe3c3812985
alex <alex.emelyanov.ua@gmail.com>
2013-06-13 13:57:44 +0300

    In progress Story-1668

--- src/CreditJeeves/AdminBundle/Admin/LeadAdmin.php
+++ src/CreditJeeves/AdminBundle/Admin/LeadAdmin.php
@@ -5,6 +5,11 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Knp\Menu\ItemInterface as MenuItemInterface;
+use Knp\Menu\FactoryInterface as MenuFactoryInterface;
 
 class LeadAdmin extends Admin
 {
@@ -35,22 +40,59 @@ class LeadAdmin extends Admin
      */
     public function createQuery($context = 'list')
     {
-        $nUserId = $this->getRequest()->get('user_id', null);
-        $nGroupId = $this->getRequest()->get('group_id', null);
+        $nUserId = $this->getRequest()->get('user_id', $this->request->getSession()->get('user_id', null));
+        $nGroupId = $this->getRequest()->get('group_id', $this->request->getSession()->get('group_id', null));
         $query = parent::createQuery($context);
         $alias = $query->getRootAlias();
         if (!empty($nUserId)) {
+            $this->request->getSession()->set('user_id', $nUserId);
             $query->andWhere($alias.'.cj_account_id = :user_id');
             $query->setParameter('user_id', $nUserId);
         }
         if (!empty($nGroupId)) {
+            $this->request->getSession()->set('group_id', $nGroupId);
             $query->andWhere($alias.'.cj_group_id = :group_id');
             $query->setParameter('group_id', $nGroupId);
         }
         return $query;
     }
-    
-    
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add(
+                'user.full_name',
+                'text',
+                array(
+                    'label' => 'Applicant'
+                )
+            )
+            ->add(
+                'dealer.full_name',
+                'text',
+                array(
+                    'label' => 'Dealer'
+                )
+            )
+            ->add(
+                'user.current_score',
+                'text',
+                array(
+                    'label' => 'Score'
+                )
+            )
+            ->addIdentifier('target_score')
+            ->add('status')
+            ->add(
+                'user.is_active',
+                'boolean',
+                array(
+                    'label' => 'Is active'
+                )
+            )
+            ->add('updatedAt', 'date');
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
@@ -60,12 +102,89 @@ class LeadAdmin extends Admin
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
+            ->add('status')
             ->add('target_score');
     }
 
-    protected function configureListFields(ListMapper $listMapper)
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureSideMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null)
     {
-        $listMapper
-            ->addIdentifier('target_score');
+//         if ('list' == $action) {
+//             $item = $this->menuFactory->createItem('back', array(
+//                 'uri' => 'javascript:void(send_test())',
+//                 'label' => 'Send test email',
+//             ));
+//             $menu->addChild($item);
+//         }
+    }
+
+    public function buildBreadcrumbs($action, MenuItemInterface $menu = null)
+    {
+        $nUserId = $this->getRequest()->get('user_id', $this->request->getSession()->get('user_id', null));
+        $nGroupId = $this->getRequest()->get('group_id', $this->request->getSession()->get('group_id', null));
+        $menu = $this->menuFactory->createItem('root');
+        $menu = $menu->addChild(
+            $this->trans(
+                $this->getLabelTranslatorStrategy()->getLabel(
+                    'dashboard',
+                    'breadcrumb',
+                    'link'
+                ),
+                array(),
+                'SonataAdminBundle'
+            ),
+            array(
+                'uri' => $this->routeGenerator->generate('sonata_admin_dashboard')
+            )
+        );
+        if ('list' == $action & !empty($nUserId)) {
+            $menu = $menu->addChild(
+                $this->trans(
+                    $this->getLabelTranslatorStrategy()->getLabel(
+                        'Dealer List',
+                        'breadcrumb',
+                        'link'
+                    ),
+                    array(),
+                    'SonataAdminBundle'
+                ),
+                array(
+                    'uri' => $this->routeGenerator->generate('admin_cj_dealer_list')
+                )
+            );
+        }
+        if ('list' == $action & !empty($nGroupId)) {
+            $menu = $menu->addChild(
+                $this->trans(
+                    $this->getLabelTranslatorStrategy()->getLabel(
+                        'Group List',
+                        'breadcrumb',
+                        'link'
+                    ),
+                    array(),
+                    'SonataAdminBundle'
+                ),
+                array(
+                    'uri' => $this->routeGenerator->generate('admin_cj_group_list')
+                )
+            );
+        }
+        $menu = $menu->addChild(
+            $this->trans(
+                $this->getLabelTranslatorStrategy()->getLabel(
+                    'Lead List',
+                    'breadcrumb',
+                    'link'
+                ),
+                array(),
+                'SonataAdminBundle'
+            ),
+            array(
+                'uri' => $this->routeGenerator->generate('admin_cj_lead_list')
+            )
+        );
+        return $this->breadcrumbs[$action] = $menu;
     }
 }
