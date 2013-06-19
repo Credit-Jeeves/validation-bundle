78c29ee0abf98c5423f24d2ef0e17b330b4edb79
Ton Sharp <66ton99@gmail.com>
2013-06-19 19:25:06 +0300

    Updated README

--- app/AppCjKernel.php
+++ app/AppCjKernel.php
@@ -49,11 +49,13 @@ class AppCjKernel extends AppKernel
             new CreditJeeves\ApplicantBundle\ApplicantBundle(),
             new CreditJeeves\CheckoutBundle\CheckoutBundle(),
         );
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'cli'))) {
+            $bundles[] = new Khepin\YamlFixturesBundle\KhepinYamlFixturesBundle();
+        }
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Khepin\YamlFixturesBundle\KhepinYamlFixturesBundle();
         }
         if (in_array($this->getEnvironment(), array('test'))) {
             $bundles[] = new Behat\MinkBundle\MinkBundle();
