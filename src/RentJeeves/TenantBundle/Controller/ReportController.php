e7849e76151b18f0cb4a067b167f23311e580a68
alex <alex.emelyanov.ua@gmail.com>
2013-07-01 17:42:51 +0300

    In progress Story-1604

---
+++ src/RentJeeves/TenantBundle/Controller/ReportController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RentJeeves\TenantBundle\Controller;
+
+use CreditJeeves\DataBundle\Entity\Operation;
+use CreditJeeves\DataBundle\Entity\Order;
+use CreditJeeves\DataBundle\Entity\User;
+use CreditJeeves\CoreBundle\Controller\ApplicantController as Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ReportController extends Controller
+{
+    /**
+     * @Route("/report", name="tenant_report")
+     * @Template()
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        // TODO add check for admin
+        /* @var User $User */
+        $User = $this->getUser();
+
+        /** @var Order $Order */
+        if ($Order = $User->getLastCompleteOrder()) {
+            /** @var Operation $Operation */
+            if ($Operation = $Order->getOperations()->last()) {
+                $Report = $Operation->getReportD2c();
+            } else {
+                return $this->redirect($this->generateUrl('core_report_get_d2c'));
+            }
+        } else {
+            return $this->createNotFoundException('Order does not found');
+        }
+
+        return array(
+            'Report' => $Report,
+            'sSupportEmail' => $this->container->getParameter('support_email'),
+            'sSupportPhone' => $this->container->getParameter('support_phone'),
+        );
+    }
+}
