a92247eac1861ba8fa82e30bd0230df8aea08d6d
alex <alex.emelyanov.ua@gmail.com>
2013-07-05 14:17:45 +0300

    In progress properties

--- src/CreditJeeves/CoreBundle/Resources/public/js/google-maps-address.js
+++ src/CreditJeeves/CoreBundle/Resources/public/js/google-maps-address.js
@@ -1,7 +1,7 @@
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(-33.8688, 151.2195),
-        zoom: 13,
+        center: new google.maps.LatLng(38, -90),
+        zoom: 4,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(
@@ -15,59 +15,77 @@ function initialize() {
     var marker = new google.maps.Marker({
             map: map
     });
+    
+    function validateAddress(){
+      infowindow.close();
+      marker.setVisible(false);
+      input.className = '';
+      var place = autocomplete.getPlace();
+      //Inform the user that the place was not found and return.
+      if (!place.geometry) {
+          input.className = 'notfound';
+          return;
+      }
+      //If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+      } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(15);  // Why 17? Because it looks good.
+      }
+      marker.setIcon(/** @type {google.maps.Icon} */({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+      var address = '';
+      if (place.address_components) {
+          address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || '')
+    //        (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+      }
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    }
 
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
-    input.className = '';
-    var place = autocomplete.getPlace();
-    //Inform the user that the place was not found and return.
-    if (!place.geometry) {
-        input.className = 'notfound';
-        return;
-    }
-    //If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-    } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
-    }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-    var address = '';
-    if (place.address_components) {
-        address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || '')
-//          (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-    }
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
+    $('#property-search').change(function(){
+      $(this).addClass('notfound');
     });
 
-$('#property-add').click(function(){
-  
-  if('notfound' == $('#property-search').attr('class')) {
-        alert('Such address doesn\'t exist!');
-        return false;
-    }
-    if ('' == $('#property-search').val()) {
-      alert('Property Address empty');
-      return false;
-    }
-    alert($('#property-search').val());
-});
+    $('#property-add').click(function(){
+      if('notfound' == $('#property-search').attr('class')) {
+            alert('Such address doesn\'t exist!');
+            return false;
+        }
+        if ('' == $('#property-search').val()) {
+          alert('Property Address empty');
+          return false;
+        }
+        var place = autocomplete.getPlace();
+        data = {'data': {'address': place.address_components, 'geometry': place.geometry}};
+        jQuery.ajax({
+          url: Routing.generate('landlord_property_add'),
+          type: 'POST',
+          timeout: 85000, // 85 secs
+          dataType: 'json',
+          data: {'data': JSON.stringify(data, null)},
+          error: function(jqXHR, errorThrown, textStatus) {
+              //jQuery('#simulation-container').hideOverlay();
+          },
+          success: function(data, textStatus, jqXHR) {
+            
+          }
+      });
+        
+//        console.log(JSON.stringify(autocomplete.getPlace()));
+//        alert($('#property-search').val());
+    });
+    google.maps.event.addListener(autocomplete, 'place_changed', validateAddress);
 }
-
-
-
 google.maps.event.addDomListener(window, 'load', initialize);
--- src/RentJeeves/LandlordBundle/Controller/PropertyController.php
+++ src/RentJeeves/LandlordBundle/Controller/PropertyController.php
@@ -5,6 +5,8 @@ namespace RentJeeves\LandlordBundle\Controller;
 use CreditJeeves\CoreBundle\Controller\LandlordController as Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PropertyController extends Controller
 {
@@ -15,5 +17,26 @@ class PropertyController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route(
+     *  "/property/add",
+     *  name="landlord_property_add",
+     *  defaults={"_format"="json"},
+     *  requirements={"_format"="html|json"},
+     *  options={"expose"=true}
+     * )
+     * @Method({"POST"})
+     *
+     * @return array
+     */
+    public function addAction()
+    {
+        $request = $this->getRequest();
+        $address = $request->request->all('data');
+        print_r(json_decode($address['data'], true));
+        
+        return new JsonResponse(array());
     }
 }
