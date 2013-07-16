289c019a077111db00850533d21a525699714248
Sharamko Alexandr <sharamko11@yandex.ru>
2013-07-16 14:42:19 +0300

    Temp fix base test rj

--- src/RentJeeves/TestBundle/Functional/BaseTestCase.php
+++ src/RentJeeves/TestBundle/Functional/BaseTestCase.php
@@ -17,13 +17,26 @@ abstract class BaseTestCase extends Base
             return;
         }
 
-        $khepin = static::getContainer()->get('khepin.yaml_loader');
+        //@TODO: enviroment not see rj fixuture when running all test and see it if rj test only run. Need Fix it.
+        $kernel = static::getContainer()->get('kernel');
+        $env = static::getContainer()->get('kernel')->getEnvironment();
+        $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+        $application->setAutoExit(false); 
+        $options = array('command' => 'khepin:yamlfixtures:load', '--app' =>'rj', '--env'=>$env);
+        
+        if ($reload) {
+            $options['--purge-orm'] = true;
+        }
+        
+        self::$isFixturesLoaded = true;
+        $application->run(new \Symfony\Component\Console\Input\ArrayInput($options));
+/*        $khepin = static::getContainer()->get('khepin.yaml_loader');
 
         if ($reload) {
             $khepin->purgeDatabase('orm');
         }
         $khepin->loadFixtures();
-        self::$isFixturesLoaded = true;
+        self::$isFixturesLoaded = true;*/
 
         $session = $this->getMink()->getSession('goutte');
         $baseUrl = 'http://' . static::getContainer()->getParameter('server_name') . '/test.php/sfPhpunit/';
