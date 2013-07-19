59d1988244467d832697886d58611c3f1781dbdb
alex <alex.emelyanov.ua@gmail.com>
2013-07-19 13:26:53 +0300

    In progress contracts skeletone  of the component for tenant

---
+++ src/CreditJeeves/ComponentBundle/Controller/ContractsListController.php
@@ -0,0 +1,20 @@
+<?php
+namespace CreditJeeves\ComponentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ContractsListController extends Controller
+{
+    /**
+     * @Template()
+     * @return multitype:
+     */
+    public function indexAction()
+    {
+        $contracts = $this->get('core.session.applicant')->getUser()->getContracts();
+        return array(
+            'contracts' => $contracts,
+        );
+    }
+}
