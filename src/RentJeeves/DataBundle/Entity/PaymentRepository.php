659866bf7a4de4d2cfe7f9ba21608e9c7e91e998
alex <alex.emelyanov.ua@gmail.com>
2013-07-25 12:55:22 +0300

    Fixed model

--- src/RentJeeves/DataBundle/Entity/PaymentRepository.php
+++ src/RentJeeves/DataBundle/Entity/PaymentRepository.php
@@ -2,7 +2,7 @@
 namespace RentJeeves\DataBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
-use RentJeeves\DataBundle\Enum\ContractStatus;
+use RentJeeves\DataBundle\Enum\PaymentStatus;
 
 class PaymentRepository extends EntityRepository
 {
