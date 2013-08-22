b6f7cc11e071142c7f45f1db076961ce24cb0134
Sharamko Alexandr <sharamko11@yandex.ru>
2013-08-22 13:40:02 +0300

    Work on sorting in process

--- src/RentJeeves/ComponentBundle/Resources/public/js/payments.js
+++ src/RentJeeves/ComponentBundle/Resources/public/js/payments.js
@@ -8,6 +8,8 @@ function Payments() {
   this.current = ko.observable(1);
   this.last = ko.observable('Last');
   this.processPayment = ko.observable(true);
+  this.sortColumn = ko.observable("status");
+  this.isSortAsc = ko.observable(true);
   this.ajaxAction = function() {
     self.processPayment(true);
     $.ajax({
@@ -17,7 +19,9 @@ function Payments() {
       data: {
         'data': {
           'page' : self.current(),
-          'limit' : limit
+          'limit' : limit,
+          'sortColumn': self.sortColumn(),
+          'isSortAsc': self.isSortAsc()
         }
       },
       success: function(response) {
@@ -26,6 +30,14 @@ function Payments() {
         self.aPayments(response.payments);
         self.total(response.total);
         self.pages(response.pagination);
+        if(self.sortColumn().length == 0) {
+          return;
+        }
+        if(self.isSortAsc()) {
+          $('#'+self.sortColumn()).attr('class', 'sort-dn');
+        } else {
+          $('#'+self.sortColumn()).attr('class', 'sort-up');
+        }
       }
     });
   };
@@ -41,6 +53,26 @@ function Payments() {
       self.current(Math.ceil(self.total()/limit));
     }
     self.ajaxAction();
+  };
+  this.sortIt = function(data, event) {
+     field = event.target.id;
+
+     if(field.length == 0) {
+        return;
+     }
+     self.sortColumn(field);
+     $('.sort-dn').attr('class', 'sort');
+     $('.sort-up').attr('class', 'sort');
+     if(self.isSortAsc() === false) {
+      self.isSortAsc(true);
+      $('#'.field).attr('class', 'sort-dn');
+     } else {
+      self.isSortAsc(false);
+      $('#'.field).attr('class', 'sort-up');
+     }
+     
+     self.current(1);
+     self.ajaxAction();
   };
 }
 
