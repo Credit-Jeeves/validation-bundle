5dd078cfc14361c3a41ed458d3756b7175700c4b
Sharamko Alexandr <sharamko11@yandex.ru>
2013-08-23 14:29:43 +0300

    Add select box for status search on the landlord dashboard

--- src/RentJeeves/ComponentBundle/Resources/public/js/payments.js
+++ src/RentJeeves/ComponentBundle/Resources/public/js/payments.js
@@ -15,30 +15,6 @@ function Payments() {
   this.isSearch = ko.observable(false);
   this.notHaveResult = ko.observable(false);
 
-  this.search = function() {
-    var searchCollum = $('#searchPayments').linkselect('val');
-    if(typeof searchCollum != 'string') {
-       searchCollum = '';
-    }
-    if(self.searchText().length <= 0) {
-      $('#searsh-field-payments').css('border-color', 'red');
-      return;
-    } else {
-      $('#searsh-field-payments').css('border-color', '#bdbdbd');
-    }
-    self.isSearch(true);
-    self.searchCollum(searchCollum);
-    self.current(1);
-    self.ajaxAction();
-  }
-
-  this.clearSearch = function() {
-    self.searchText('');
-    self.searchCollum('');
-    self.current(1);
-    self.ajaxAction();
-    self.isSearch(false);
-  }
   this.ajaxAction = function() {
     self.processPayment(true);
     $.ajax({
@@ -71,7 +47,41 @@ function Payments() {
         }
       }
     });
-  };
+  }
+
+  this.search = function() {
+    var searchCollum = $('#searchPayments').linkselect('val');
+    if(typeof searchCollum != 'string') {
+       searchCollum = '';
+    }
+    if(searchCollum != 'status') {
+      if(self.searchText().length <= 0) {
+        $('#searsh-field-payments').css('border-color', 'red');
+        return;
+      } else {
+        $('#searsh-field-payments').css('border-color', '#bdbdbd');
+      }
+    } else {
+      var searchText = $('#searchPaymentsStatus').linkselect('val');
+      if(typeof searchText != 'string') {
+         searchText = '';
+      }
+      self.searchText(searchText);
+    }
+    self.isSearch(true);
+    self.searchCollum(searchCollum);
+    self.current(1);
+    self.ajaxAction();
+  }
+
+  this.clearSearch = function() {
+    self.searchText('');
+    self.searchCollum('');
+    self.current(1);
+    self.ajaxAction();
+    self.isSearch(false);
+  }
+
   this.countPayments = ko.computed(function(){
     return parseInt(self.aPayments().length);
   });
@@ -84,7 +94,7 @@ function Payments() {
       self.current(Math.ceil(self.total()/limit));
     }
     self.ajaxAction();
-  };
+  }
   this.sortIt = function(data, event) {
      field = event.target.id;
 
@@ -104,7 +114,7 @@ function Payments() {
      
      self.current(1);
      self.ajaxAction();
-  };
+  }
 }
 
 var PaymentsViewModel = new Payments();
@@ -113,5 +123,16 @@ $(document).ready(function(){
   ko.applyBindings(PaymentsViewModel, $('#payments-block').get(0));
   PaymentsViewModel.ajaxAction();
   $('#searchPayments').linkselect("destroy");
-  $('#searchPayments').linkselect();
+  $('#searchPayments').linkselect({
+    change: function(li, value, text){
+      PaymentsViewModel.searchText('');
+      if(value == 'status') {
+        $('#searchSelect').show();
+        $('#searchInput').hide();
+      } else {
+        $('#searchSelect').hide();
+        $('#searchInput').show();
+      }
+    }
+  });
 });
