dc9741d4c20b00c47e7964410d60322fc185d7fb
alex <alex.emelyanov.ua@gmail.com>
2013-08-28 14:09:28 +0300

    In progress Story-1802 fixing tests

--- src/RentJeeves/TenantBundle/Controller/IndexController.php
+++ src/RentJeeves/TenantBundle/Controller/IndexController.php
@@ -19,17 +19,17 @@ class IndexController extends Controller
         $em = $this->get('doctrine')->getManager();
         $emContract = $em->getRepository('RjDataBundle:Contract');
         $allContracts = $emContract->getCountByStatus($tenant);
+        
         $pendingContracts = $emContract->getCountByStatus($tenant, ContractStatus::PENDING);
         $activeContracts = $emContract->getCountByStatus($tenant, ContractStatus::CURRENT);
-
+        
         if ($allContracts === $pendingContracts) {
-            $status = ContractStatus::NEWONE;
+            $status = ContractStatus::PENDING;
         } elseif ($activeContracts > 0) {
             $status = ContractStatus::CURRENT;
         } else {
             $status = ContractStatus::APPROVED;
         }
-
         $isReporting = $activeContracts = $em->getRepository('RjDataBundle:Contract')
                                 ->countReporting($tenant);
         return array(
