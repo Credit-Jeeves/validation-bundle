9517d778900739d6379d350dbb40028850ab0995
Sharamko Alexandr <sharamko11@yandex.ru>
2013-11-05 11:32:25 +0200

    RT-11 in process

--- src/RentJeeves/PublicBundle/Resources/public/js/public.index.js
+++ src/RentJeeves/PublicBundle/Resources/public/js/public.index.js
@@ -66,57 +66,125 @@ $(document).ready(function(){
         }
 
         $('#property-search').change(function(){
+          console.info('property search changed');
           $(this).addClass('notfound');
         });
 
-        $('#property-add').click(function(){
-            if(ERROR == $('#property-search').attr('class')) {
+/*        $("#property-search").focusin(function () {
+            $(document).keypress(function (e) {
+                if (e.which == 13) {
+                    infowindow.close();
+                    var firstResult = $(".pac-container .pac-item:first").text();
+                    var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({"address":firstResult }, function(results, status) {
+                        console.info(results);
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            var lat = results[0].geometry.location.lat(),
+                                lng = results[0].geometry.location.lng(),
+                                placeName = results[0].address_components[0].long_name,
+                                latlng = new google.maps.LatLng(lat, lng);
+
+                            marker.setPosition(latlng);
+                            infowindow.setContent(placeName);
+                            infowindow.open(map, marker);
+                            $("#property-search").val(firstResult);
+                        }
+                    });
+                }
+            });
+        });*/
+
+        function executeSearch(data)
+        {
+            console.info('Туц');
+            console.info(data);
+            if (typeof data == 'undefined') {
                 showError(Translator.get('select.from.drop.down.list'));
+                //console.info('3');
                 return false;
             }
-            if ('' == $('#property-search').val()) {
-              showError(Translator.get('error.property.empty'));
-              return false;
-            }
-            var place = autocomplete.getPlace();
-            if (typeof place == 'undefined') {
-                showError(Translator.get('select.from.drop.down.list'));
-                return false;
-            }
-            var data = {'address': place.address_components, 'geometry':place.geometry};
+
 
             if($('#property-add').hasClass('grey')) {
-              return false;
+                return false;
             }
 
             $('#property-add').addClass('disabled grey');
             $('#property-add').find('.loadingSpinner').show();
 
+            //console.info(place);
+            //console.info(data);
             jQuery.ajax({
-              url: Routing.generate('landlord_property_add'),
-              type: 'POST',
-              dataType: 'json',
-              data: {'data': JSON.stringify(data, null)},
-              error: function(jqXHR, errorThrown, textStatus) {
-                //location.href = Routing.generate('landlord_properties');
-              },
-              success: function(data, textStatus, jqXHR) {
-                var isInIFrame = (window.location != window.parent.location);
-                var location = Routing.generate('iframe_search_check', {'propertyId':data.property.id});
+                url: Routing.generate('landlord_property_add'),
+                type: 'POST',
+                dataType: 'json',
+                data: {'data': JSON.stringify(data, null)},
+                error: function(jqXHR, errorThrown, textStatus) {
+                    //location.href = Routing.generate('landlord_properties');
+                },
+                success: function(data, textStatus, jqXHR) {
+                    var isInIFrame = (window.location != window.parent.location);
+                    var location = Routing.generate('iframe_search_check', {'propertyId':data.property.id});
 
-                if (data.isLogin && data.isLandlord) {
-                    var location = Routing.generate('landlord_properties');
-                }
+                    if (data.isLogin && data.isLandlord) {
+                        var location = Routing.generate('landlord_properties');
+                    }
 
-                if (isInIFrame == true) {
-                    // iframe
-                    window.parent.location.href = location;
-                } else {
-                    // no iframe
-                    window.location.href = location;
+                    if (isInIFrame == true) {
+                        // iframe
+                        //window.parent.location.href = location;
+                    } else {
+                        // no iframe
+                        //window.location.href = location;
+                    }
                 }
-              }
             });
+        }
+
+        $('#property-add').click(function(){
+
+/*            if(ERROR == $('#property-search').attr('class')) {
+                showError(Translator.get('select.from.drop.down.list'));
+                console.info('1');
+                return false;
+            }*/
+
+            if ('' == $('#property-search').val()) {
+                showError(Translator.get('error.property.empty'));
+                return false;
+            }
+
+            if (typeof data != 'undefined') {
+                delete data;
+            }
+
+            var place = autocomplete.getPlace();
+            if (typeof place != 'undefined') {
+                var data = {'address': place.address_components, 'geometry':place.geometry};
+                executeSearch(data);
+            } else {
+                infowindow.close();
+                var firstResult = $(".pac-container .pac-item:first").text();
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({"address":firstResult }, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var lat = results[0].geometry.location.lat(),
+                            lng = results[0].geometry.location.lng(),
+                            placeName = results[0].address_components[0].long_name,
+                            latlng = new google.maps.LatLng(lat, lng);
+
+                        marker.setPosition(latlng);
+                        infowindow.setContent(placeName);
+                        infowindow.open(map, marker);
+                        $("#property-search").val(firstResult);
+                        var data = {'address': results[0].address_components, 'geometry':results[0].geometry};
+                        executeSearch(data);
+                    } else {
+                        showError(Translator.get('select.from.drop.down.list'));
+                    }
+                });
+            }
+
         });
 
         google.maps.event.addListener(autocomplete, 'place_changed', validateAddress);
