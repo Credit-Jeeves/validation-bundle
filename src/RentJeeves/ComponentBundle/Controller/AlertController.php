88fe7641772efc599e43f8d8f6007238d5fa0953
alex <alex.emelyanov.ua@gmail.com>
2013-11-11 13:31:51 +0200

    Bugfixes in progress

--- src/RentJeeves/ComponentBundle/Controller/AlertController.php
+++ src/RentJeeves/ComponentBundle/Controller/AlertController.php
@@ -2,8 +2,8 @@
 
 namespace RentJeeves\ComponentBundle\Controller;
 
-//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use RentJeeves\CoreBundle\Controller\LandlordController as Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use RentJeeves\CoreBundle\Controller\LandlordController as Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use RentJeeves\DataBundle\Enum\ContractStatus;
 
