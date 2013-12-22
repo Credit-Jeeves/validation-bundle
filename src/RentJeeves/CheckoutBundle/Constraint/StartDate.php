324affceb79dfdbb074a24e5ae8a082ad3c36fff
Sharamko Alexandr <sharamko11@yandex.ru>
2013-12-22 11:44:42 +0200

    RT-178

--- src/RentJeeves/CheckoutBundle/Constraint/StartDate.php
+++ src/RentJeeves/CheckoutBundle/Constraint/StartDate.php
@@ -11,8 +11,7 @@ class StartDate extends Constraint
 {
     public $oneTimeUntilValue;
 
-    public $message = 'Please check your dates. It appears you have set up a recurring payment to start in the past.
-                       You must start it today or in the future.';
+    public $message = 'payment.start_date.error';
 
     public function validatedBy()
     {
