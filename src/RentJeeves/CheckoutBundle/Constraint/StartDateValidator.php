00dbbe6833669ca8a780cb790cd87c03e89387b2
Sharamko Alexandr <sharamko11@yandex.ru>
2014-01-24 11:26:34 +0200

    RT-239

--- src/RentJeeves/CheckoutBundle/Constraint/StartDateValidator.php
+++ src/RentJeeves/CheckoutBundle/Constraint/StartDateValidator.php
@@ -10,10 +10,24 @@ class StartDateValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-        $now = new DateTime($constraint->oneTimeUntilValue);
-        $date = DateTime::createFromFormat('Y-m-d', $value);
-        if ($now < $date) {
+        $nowDateTime = new DateTime();
+        $dateValidation = DateTime::createFromFormat('Y-m-d', $value);
+
+        if ($dateValidation > $nowDateTime) {
             return;
+        }
+
+        /**
+         * If it's today, need check time
+         */
+        if ($dateValidation->format('Y-m-d') === $nowDateTime->format('Y-m-d')) {
+            $nowDateTimeWithCrontabTimeExecution = new DateTime($constraint->oneTimeUntilValue);
+            $timeExecution = (int) $nowDateTimeWithCrontabTimeExecution->format('Hmi');
+            $timeValidation = (int) $dateValidation->format('Hmi');
+
+            if ($timeExecution > $timeValidation) {
+                return;
+            }
         }
 
         $this->context->addViolation($constraint->message);
