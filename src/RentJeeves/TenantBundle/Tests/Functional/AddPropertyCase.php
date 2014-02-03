6da975433cc6cdc872af617828d3a858e3dcb567
Sharamko Alexandr <sharamko11@yandex.ru>
2014-02-03 17:34:17 +0200

    Fix for stupid selenium

--- src/RentJeeves/TenantBundle/Tests/Functional/AddPropertyCase.php
+++ src/RentJeeves/TenantBundle/Tests/Functional/AddPropertyCase.php
@@ -100,7 +100,7 @@ class AddPropertyCase extends BaseTestCase
         $this->assertNotNull($propertySearch = $this->page->find('css', '#search-submit'));
         $propertySearch->click();
         $this->session->wait($this->timeout+5000, "window.location.pathname.match('\/property\/add\/[0-9]') != null");
-        $this->session->wait($this->timeout+5000, "typeof jQuery != 'undefined'");
+        $this->session->wait($this->timeout+10000, "typeof jQuery != 'undefined'");
         $this->assertNotNull($form = $this->page->find('css', '#formSearch'));
         $this->fillForm(
             $form,
