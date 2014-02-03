1e63d44bd849396f0960113f25775ba774176566
Sharamko Alexandr <sharamko11@yandex.ru>
2014-02-03 14:44:09 +0200

    RT-224

---
+++ src/CreditJeeves/DataBundle/EventListener/LeadListener.php
@@ -0,0 +1,70 @@
+<?php
+namespace CreditJeeves\DataBundle\EventListener;
+
+use CreditJeeves\CoreBundle\Mailer\Mailer;
+use CreditJeeves\DataBundle\Entity\Lead;
+use CreditJeeves\DataBundle\Enum\LeadStatus;
+use CreditJeeves\DataBundle\Enum\UserType;
+use CreditJeeves\DataBundle\Entity\Score;
+//use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use JMS\DiExtraBundle\Annotation\Service;
+use JMS\DiExtraBundle\Annotation\Tag;
+use JMS\DiExtraBundle\Annotation\InjectParams;
+use JMS\DiExtraBundle\Annotation\Inject;
+
+
+/**
+ * @Service("lead.event_listener.doctrine")
+ * @Tag(
+ *     "doctrine.event_listener",
+ *     attributes = {
+ *         "event"="preUpdate",
+ *         "method"="preUpdate"
+ *     }
+ * )
+ */
+class LeadListener
+{
+    protected $container;
+
+    /**
+     * @InjectParams({
+     *     "container" = @Inject("service_container")
+     * })
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * We need send email to Applicant when his Applicant change status from READY to ACTIVE
+     *
+     * @param LifecycleEventArgs $eventArgs
+     */
+    public function preUpdate(PreUpdateEventArgs $eventArgs)
+    {
+        $em = $eventArgs->getEntityManager();
+        $lead = $eventArgs->getEntity();
+        $id = $lead->getId();
+
+        if (!($lead instanceof Lead)) {
+            return;
+        }
+
+        if (!$eventArgs->hasChangedField('status')) {
+            return;
+        }
+
+        if ($eventArgs->getNewValue('status') !== LeadStatus::READY) {
+            return;
+        }
+
+        if ($eventArgs->getOldValue('status') !== LeadStatus::ACTIVE) {
+            return;
+        }
+        //@todo send email to dealer, will be created seperated task
+        $this->container->get('project.mailer')->sendTargetApplicant($lead);
+    }
+}
