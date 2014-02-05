07cf2a223ad0dab5239196002e41f8975d7d8bfe
Sharamko Alexandr <sharamko11@yandex.ru>
2014-02-05 19:22:51 +0200

    RT-273

---
+++ src/CreditJeeves/ExperianBundle/Model/GLBRule11.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CreditJeeves\ExperianBundle\Model;
+
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @Serializer\XmlRoot("GLBRule11")
+ */
+class GLBRule11 extends GLBRule
+{
+}
