4cb71402b5275c89a8683833e57026990e82d1a0
Sharamko Alexandr <sharamko11@yandex.ru>
2014-02-27 15:03:51 +0200

    RT-276

---
+++ src/CreditJeeves/UserBundle/Resources/public/js/reject.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    $.reject({
+        reject: {
+            msie5: true,
+            msie6: true,
+            msie7: true,
+            msie8: true
+        },
+        imagePath: '/bundles/user/img/',
+        closeCookie: false,//true,
+        // Header of pop-up window
+        header: Translator.trans('reject.header'),
+        // Paragraph 1
+        paragraph1: Translator.trans('reject.paragraph1'),
+        // Paragraph 2
+        paragraph2: Translator.trans('reject.paragraph2'),
+        close: true, // Allow closing of window
+        // Message displayed below closing link
+        closeMessage: Translator.trans('reject.closeMessage'),
+        closeLink: Translator.trans('reject.closeWindow')
+    });
+});
