5f32dbfd6af7d997fcf835c3eb8ef69f78588959
Ton Sharp <66Ton99@gmail.com>
2014-03-12 15:50:33 +0200

    Update jenkins.sh

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -33,7 +33,7 @@ fi
 
 echo "##### RUN PHPUNIT ALL TESTS #####"
 #nice -n 5
-php -C -q -d memory_limit=1024M $PHPUNIT_PATH --debug -v \
+php -C -q -d memory_limit=1280M $PHPUNIT_PATH --debug -v \
   $PHPUNIT_PARAMS \
   --log-junit=$BUILD_DIR/allTests.xml
 
