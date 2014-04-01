61998cb80a78f082982c1a3e14e089c71b37b06c
66Ton99 <66ton99@gmail.com>
2014-04-01 17:38:20 +0300

    Fixed: critical bugs & build

--- src/RentJeeves/DataBundle/Tests/Entity/PaymentCase.php
+++ src/RentJeeves/DataBundle/Tests/Entity/PaymentCase.php
@@ -16,13 +16,9 @@ class PaymentCase extends PHPUnit_Framework_TestCase
             array(31, '2014-01-15', '2014-01-31'),
             array(30, '2014-02-27', '2014-02-28'),
             array(1, '2014-02-27', '2014-03-01'),
+            array(1, '2014-03-31', '2014-04-01'),
         );
     }
-
-    /**
-     * @test
-     * @dataProvider providerForgetNextPaymentDate
-     */
     public function getNextPaymentDate($dueDate, $now, $will)
     {
         /** @var Payment $payment */
@@ -39,5 +35,19 @@ class PaymentCase extends PHPUnit_Framework_TestCase
         $payment->setDueDate($dueDate);
 
         $this->assertEquals(new DateTime($will), $payment->getNextPaymentDate());
+    }
+
+    /**
+     * @test
+     * @dataProvider providerForgetNextPaymentDate
+     */
+    public function getNextPaymentDateWithDifferentTimezones($dueDate, $now, $will)
+    {
+        \date_default_timezone_set('Europe/Kiev');
+        $this->getNextPaymentDate($dueDate, $now, $will);
+        \date_default_timezone_set('America/New_York');
+        $this->getNextPaymentDate($dueDate, $now, $will);
+        \date_default_timezone_set('GMT');
+        $this->getNextPaymentDate($dueDate, $now, $will);
     }
 }
