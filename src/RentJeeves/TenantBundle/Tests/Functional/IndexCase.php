545d1a561836c6eb7ce8dd70130e076166d67029
Tatyana <shtatalex@gmail.com>
2014-04-09 13:49:24 +0300

    Added test

--- src/RentJeeves/TenantBundle/Tests/Functional/IndexCase.php
+++ src/RentJeeves/TenantBundle/Tests/Functional/IndexCase.php
@@ -9,7 +9,7 @@ class IndexCase extends BaseTestCase
     /**
      * @test
      */
-    public function paymentsHistory()
+    public function existPaymentsHistory()
     {
         $this->load(true);
         $this->setDefaultSession('selenium2');
@@ -17,5 +17,19 @@ class IndexCase extends BaseTestCase
         $this->assertNotNull($paymentsTable = $this->page->find('css', '#tenant-payments'));
         $this->assertNotNull($payments = $this->page->findAll('css', '#tenant-payments table>tbody>tr'));
         $this->assertEquals(36, count($payments));
+        $this->logout();
+    }
+
+    /**
+     * @test
+     */
+    public function emptyPaymentsHistory()
+    {
+        $this->load(true);
+        $this->setDefaultSession('selenium2');
+        $this->login('mamazza@rentrack.com', 'pass');
+        $this->assertNotNull($paymentsTable = $this->page->find('css', '#tenant-payments'));
+        $this->assertFalse($paymentsTable->isVisible());
+        $this->assertEquals(0, count($this->page->findAll('css', '#tenant-payments table>tbody>tr')));
     }
 }
