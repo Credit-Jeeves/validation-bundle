c7862c976873e14e1fa53fd2528b2c3a6b4f947f
Sharamko Alexandr <sharamko11@yandex.ru>
2014-04-11 17:49:10 +0300

    RT-346

--- app/DoctrineMigrations/Version20140411154555.php
+++ app/DoctrineMigrations/Version20140411154555.php
@@ -5,14 +5,10 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20140411154555 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             $this->connection->getDatabasePlatform()->getName() != "mysql",
             "Migration can only be executed safely on 'mysql'."
@@ -44,7 +40,6 @@ class Version20140411154555 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             $this->connection->getDatabasePlatform()->getName() != "mysql",
             "Migration can only be executed safely on 'mysql'."
