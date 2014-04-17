dcb82e22df2bbfad6c5dfb2b3b985c0e03ecba2f
Sharamko Alexandr <sharamko11@yandex.ru>
2014-04-17 14:45:06 +0300

    RT-375 implement Tatyana notice

--- src/RentJeeves/LandlordBundle/Form/ImportResidentMappingType.php
+++ src/RentJeeves/LandlordBundle/Form/ImportResidentMappingType.php
@@ -7,8 +7,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-use CreditJeeves\CoreBundle\Translation\Translator;
-
 /**
  * This form for Unit
  *
