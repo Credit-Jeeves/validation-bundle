9083ed6587eafa3eac2f0191c6ebf835258c9456
Sharamko Alexandr <sharamko11@yandex.ru>
2014-04-23 16:04:22 +0300

    RT-375

--- src/RentJeeves/DataBundle/Entity/Unit.php
+++ src/RentJeeves/DataBundle/Entity/Unit.php
@@ -4,6 +4,7 @@ namespace RentJeeves\DataBundle\Entity;
 use RentJeeves\DataBundle\Model\Unit as Base;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use \Doctrine\ORM\Event\LifecycleEventArgs;
 
 /**
  * Property
@@ -11,11 +12,33 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @ORM\Entity(repositoryClass="RentJeeves\DataBundle\Entity\UnitRepository")
  * @ORM\Table(name="rj_unit")
  * @Gedmo\SoftDeleteable(fieldName="deletedAt")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Unit extends Base
 {
     public function __toString()
     {
         return $this->getName();
+    }
+
+    /**
+     * @TODO need test for it.
+     * Documentation link https://credit.atlassian.net/wiki/display/RT/Tenant+Waiting+Room
+     * @ORM\PostRemove
+     */
+    public function deleteAllWaitingContract(LifecycleEventArgs $args)
+    {
+        $contractsWaiting = $this->getContractsWaiting();
+        $em = $args->getEntityManager();
+
+        if (empty($contractsWaiting)) {
+            return;
+        }
+
+        foreach ($contractsWaiting as $contractWaiting) {
+            $em->remove($contractWaiting);
+        }
+
+        $em->flush();
     }
 }
