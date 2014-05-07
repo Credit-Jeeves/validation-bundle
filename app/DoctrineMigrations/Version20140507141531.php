93e23d40a5821d5d7e4264ae3f5c4ae2b9cced37
Sharamko Alexandr <sharamko11@yandex.ru>
2014-05-07 16:38:34 +0300

    RT-403

--- app/DoctrineMigrations/Version20140507141531.php
+++ app/DoctrineMigrations/Version20140507141531.php
@@ -14,28 +14,29 @@ class Version20140507141531 extends AbstractMigration
             "Migration can only be executed safely on 'mysql'."
         );
         $template = <<<EOT
-        {% extends "RjComponentBundle:Mailer:base.html.twig" %}
-        {% block h1 %}Dear {{ landlordFirstName }},{% endblock %}
-        {% block email %}
-        {% if orderStatus == 'refunded' %}
-        Per your tenant's({{ tenantName }}) request, their rent of {{ rentAmount }} sent on {{ orderDate }} was refunded
-        and will be deducted from your account within a couple of days. Please contact your tenant
-        if you have any questions regarding this refund.
-        {% elseif orderStatus == 'cancelled' %}
-        Per your your tenant's({{ tenantName }}) request, their rent payment of {{ rentAmount }} sent on {{ orderDate }}
-        was cancelled. You will not see a deposit in your account since it was cancelled before
-        payment settlement. Please contact your tenant if you have any questions regarding this cancellation.
-        {% else %}
-        Your tenant's({{ tenantName }}) payment of {{ rentAmount }} sent on {{ orderDate }} was returned. This amount
-        has been deducted from your account per the RentTrack terms of service. Your rent is currently not paid.
-        Please contact your tenant if to arrange another payment.
+{% extends 'RjComponentBundle:Mailer:base.html.twig' %}
+{% block h1 %}Dear {{ landlordFirstName }},{% endblock %}
+{% block email %}
+    {% if orderStatus == 'refunded' %}
 
+    At the request of {{ tenantName }}, their rent of {{ rentAmount }} sent on {{ orderDate }} was refunded.
+    Any monies already deposited will be deducted from your account within a couple of days.
+    Please contact your tenant if you have any questions regarding this refund.
+    {% elseif orderStatus == 'cancelled' %}
 
-        RentTrack Customer Support will also reach out to your tenant to see if their payment source information
-        can be corrected.
-        {% endif %}
-        If you have any other questions, please contact help@renttrack.com or call 866-841-9090 x2.
-        {% endblock %}
+    At the request of {{ tenantName}}, their rent payment of {{ rentAmount }} sent on {{ orderDate }}
+    was cancelled. You will not see a deposit in your account since it was cancelled before
+    payment settlement. Please contact your tenant if you have any questions regarding this cancellation.
+    {% else %}
+
+    The rent payment by {{ tenantName }} for {{ rentAmount }} sent on {{ orderDate }} was returned.
+    Any monies already deposited  will be deducted from your account per the RentTrack terms of service.
+    Your tenant's rent is currently unpaid.
+    Your tenant may try to pay again through RentTrack, or you may arrange an alternate, immediate payment method.
+    {% endif %}
+
+    If you have any other questions, please contact help@renttrack.com or call 866-841-9090 x2.
+{% endblock %}
 
 EOT;
         $template = str_replace("'", "\'", $template);
