c3361a7faa5035c2d949fda00b794c66d79cf726
Sharamko Alexandr <sharamko11@yandex.ru>
2014-05-13 15:00:37 +0300

    RT-438 in progress

--- src/RentJeeves/LandlordBundle/Form/ContractType.php
+++ src/RentJeeves/LandlordBundle/Form/ContractType.php
@@ -42,6 +42,7 @@ class ContractType extends AbstractType
                 'error_bubbling'    => true,
             )
         );
+
         $builder->add(
             'startAt',
             'date',
@@ -52,6 +53,15 @@ class ContractType extends AbstractType
                 'error_bubbling'    => true,
             )
         );
+
+        $builder->add(
+            'dueDate',
+            'choices',
+            array(
+                'error_bubbling'    => true,
+            )
+        );
+
         $builder->add(
             'property',
             'entity',
