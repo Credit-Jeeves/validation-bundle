b9475bfe13885e3e37ccfafa1b13d6a24890839c
Stephen Crosby <stephen@brandedcrate.com>
2014-06-15 22:41:17 -0700

    use code 'RentTrackCorp'

--- app/DoctrineMigrations/Version20140405200815.php
+++ app/DoctrineMigrations/Version20140405200815.php
@@ -12,19 +12,19 @@ class Version20140405200815 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $ret = $this->addSql("INSERT INTO `cj_account_group` VALUES (NULL,NULL,NULL,NULL,NULL,'RentTrack',NULL,'RentTrack',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'flat',NULL,NULL,'generic','2014-04-05 20:13:25','2014-04-05 20:13:25')");
+        $ret = $this->addSql("INSERT INTO `cj_account_group` VALUES (NULL,NULL,NULL,NULL,NULL,'RentTrackCorp',NULL,'RentTrackCorp',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'flat',NULL,NULL,'generic','2014-04-05 20:13:25','2014-04-05 20:13:25')");
         $ret = $this->addSql("INSERT INTO `rj_deposit_account` VALUES (NULL, (
-          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrack'
-        ), 'RentTrack', 'complete', NULL)");
+          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
+        ), 'RentTrackCorp', 'complete', NULL)");
     }
 
     public function down(Schema $schema)
     {
         $ret = $this->addSql("DELETE FROM `rj_deposit_account` WHERE `group_id`=(
-          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrack'
+          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
         )");
         $this->addSql(
-          "DELETE FROM `cj_account_group` WHERE `code`='RentTrack'"
+          "DELETE FROM `cj_account_group` WHERE `code`='RentTrackCorp'"
         );
     }
 }
