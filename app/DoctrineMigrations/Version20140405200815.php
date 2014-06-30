419bbe68eb7ea853101189807c360234cd58c0f9
Stephen Crosby <stephen@brandedcrate.com>
2014-06-29 18:25:23 -0700

    name columns explicitly in migration

--- app/DoctrineMigrations/Version20140405200815.php
+++ app/DoctrineMigrations/Version20140405200815.php
@@ -12,19 +12,28 @@ class Version20140405200815 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $ret = $this->addSql("INSERT INTO `cj_account_group` VALUES (NULL,NULL,NULL,NULL,NULL,'RentTrackCorp',NULL,'RentTrackCorp',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'flat',NULL,NULL,'generic','2014-04-05 20:13:25','2014-04-05 20:13:25')");
-        $ret = $this->addSql("INSERT INTO `rj_deposit_account` VALUES (NULL, (
-          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
-        ), 'RentTrackCorp', 'complete', NULL)");
+        $ret = $this->addSql(
+            "INSERT INTO `cj_account_group`
+            (`name`, `code`, `fee_type`, `type`, `created_at`, `updated_at`)
+            VALUES
+            ('RentTrackCorp', 'RentTrackCorp', 'flat', 'generic', '2014-04-05 20:13:25', '2014-04-05 20:13:25')"
+        );
+        $ret = $this->addSql(
+            "INSERT INTO `rj_deposit_account` VALUES (NULL, (
+              SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
+            ), 'RentTrackCorp', 'complete', NULL)"
+        );
     }
 
     public function down(Schema $schema)
     {
-        $ret = $this->addSql("DELETE FROM `rj_deposit_account` WHERE `group_id`=(
-          SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
-        )");
+        $ret = $this->addSql(
+            "DELETE FROM `rj_deposit_account` WHERE `group_id`=(
+              SELECT `id` FROM `cj_account_group` WHERE `code`='RentTrackCorp'
+            )"
+        );
         $this->addSql(
-          "DELETE FROM `cj_account_group` WHERE `code`='RentTrackCorp'"
+            "DELETE FROM `cj_account_group` WHERE `code`='RentTrackCorp'"
         );
     }
 }
