ade50833132858bd0814ea94aac85117a91643e1
Yuriy Borovchuk <yurez.b@gmail.com>
2014-08-01 02:24:34 +0300

    RT-600: Added Account Number to Deposit Account Table

---
+++ app/DoctrineMigrations/Version20140731174428.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140731174428 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() != "mysql",
+            "Migration can only be executed safely on 'mysql'."
+        );
+
+        $this->addSql(
+            "ALTER TABLE rj_deposit_account
+                ADD account_number VARCHAR(255) DEFAULT NULL AFTER merchant_name"
+        );
+
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() != "mysql",
+            "Migration can only be executed safely on 'mysql'."
+        );
+
+        $this->addSql(
+            "ALTER TABLE rj_deposit_account DROP account_number"
+        );
+
+    }
+}
