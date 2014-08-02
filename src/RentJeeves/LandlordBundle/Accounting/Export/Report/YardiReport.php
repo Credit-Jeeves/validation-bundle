67751d71eb0ebd6d08d3c3d38cc333c14d620922
Alexandr Sharamko <alexandr.sharamko11@gmail.com>
2014-08-02 18:45:57 +0300

    RT-591 fix bug in zip import for Yardi

--- src/RentJeeves/LandlordBundle/Accounting/Export/Report/YardiReport.php
+++ src/RentJeeves/LandlordBundle/Accounting/Export/Report/YardiReport.php
@@ -48,10 +48,7 @@ class YardiReport extends ExportReport
 
     public function getContent($settings)
     {
-        $this->validateSettings($settings);
-        $this->setYardiParams($settings);
         $this->generateFilename($settings);
-
         $reportData = $this->getData($settings);
 
         return $this->serializer->serialize($reportData);
@@ -65,6 +62,9 @@ class YardiReport extends ExportReport
     public function getData($settings)
     {
         $this->softDeleteableControl->disable();
+
+        $this->validateSettings($settings);
+        $this->setYardiParams($settings);
 
         $beginDate = $settings['begin'].' 00:00:00';
         $endDate = $settings['end'].' 23:59:59';
