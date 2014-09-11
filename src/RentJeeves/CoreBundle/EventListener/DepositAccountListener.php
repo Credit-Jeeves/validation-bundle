d64494005e7be308d1110a77d3859bd4f91837df
Tatyana <shtatalex@gmail.com>
2014-09-11 13:05:16 +0300

    RT-621: send merchant setup email only when deposit account status is changed to COMPLETE

--- src/RentJeeves/CoreBundle/EventListener/DepositAccountListener.php
+++ src/RentJeeves/CoreBundle/EventListener/DepositAccountListener.php
@@ -8,6 +8,7 @@ use JMS\DiExtraBundle\Annotation\Tag;
 use JMS\DiExtraBundle\Annotation\Inject;
 use RentJeeves\CoreBundle\Mailer\Mailer;
 use RentJeeves\DataBundle\Entity\DepositAccount;
+use RentJeeves\DataBundle\Enum\DepositAccountStatus;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
 /**
@@ -83,7 +84,9 @@ class DepositAccountListener
             $eventArgs->setNewValue('feeCC', null);
         }
 
-        $this->sendEmail($entity);
+        if ($eventArgs->hasChangedField('status') && DepositAccountStatus::DA_COMPLETE == $entity->getStatus()) {
+            $this->sendEmail($entity);
+        }
     }
 
     private function sendEmail(DepositAccount $entity)
