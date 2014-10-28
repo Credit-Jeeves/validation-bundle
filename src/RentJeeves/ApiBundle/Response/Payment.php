04c018c4c941dde8b5ef00a4806849f5c98a787c
Cary Penniman <cary@renttrack.com>
2014-10-28 00:11:43 -0700

    implemented `GET /api/tenant/payments/{id}` support

---
+++ src/RentJeeves/ApiBundle/Response/Payment.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace RentJeeves\ApiBundle\Response;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use JMS\Serializer\Annotation as Serializer;
+use RentJeeves\ApiBundle\Services\ResourceUrlGenerator\Annotation\UrlResourceMeta;
+use RentJeeves\DataBundle\Entity\PaymentAccount as Entity;
+
+/**
+ * @DI\Service("response_resource.payment")
+ * @UrlResourceMeta(
+ *      actionName = "get_payments"
+ * )
+ */
+class Payment extends ResponseResource
+{
+    /**
+     * @var Entity
+     */
+    protected $entity;
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * TODO: This should return a URL not an ID. (RT-839)
+     *
+     * @return string
+     */
+    public function getContractUrl()
+    {
+        return $this->entity->getContract()->getId();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * @return string
+     *
+     * TODO: This should return a URL not an ID. (RT-839)
+     */
+    public function getPaymentAccountUrl()
+    {
+        return $this->entity->getPaymentAccountId();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return (string)$this->entity->getStatus();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->entity->getType();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * @return string
+     */
+    public function getRent()
+    {
+        return $this->entity->getAmount();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     *
+     * @return string
+     */
+    public function getOther()
+    {
+        return number_format($this->entity->getOther(), 2);
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     * @return integer
+     */
+    public function getDay()
+    {
+        return $this->entity->getDueDate();
+    }
+
+    /**
+     * Get startMonth
+     *
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     *
+     * @return integer
+     */
+    public function getMonth()
+    {
+        return $this->entity->getStartMonth();
+    }
+
+    /**
+     * Get startYear
+     *
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     *
+     * @return integer
+     */
+    public function getYear()
+    {
+        return $this->entity->getStartYear();
+    }
+
+    /**
+     * Get endMonth
+     *
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     *
+     * @return integer
+     */
+    public function getEndMonth()
+    {
+        return $this->entity->getEndMonth();
+    }
+
+    /**
+     * Get endYear
+     *
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     * @Serializer\Type("string")
+     *
+     * @return integer
+     */
+    public function getEndYear()
+    {
+        return $this->entity->getEndYear();
+    }
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\Groups({"PaymentDetails"})
+     *
+     * @return DateTime
+     */
+    public function getPaidFor()
+    {
+        $paidFor = $this->entity->getPaidFor();
+        $paidForFormatted = "";
+
+        if ($paidFor !== Null) {
+            $paidForFormatted = date("Y-m", $paidFor->getTimestamp());
+        }
+
+        return $paidForFormatted;
+    }
+
+
+}
---
+++ src/RentJeeves/ApiBundle/Tests/RestApi/Tenant/PaymentsControllerCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace RentJeeves\ApiBundle\Tests\RestApi\Tenant;
+
+use JMS\Serializer\Serializer;
+use RentJeeves\ApiBundle\Tests\BaseApiTestCase;
+use RentJeeves\DataBundle\Entity\Payment;
+
+class PaymentControllerCase extends BaseApiTestCase
+{
+
+    public function testShow()
+    {
+        $id = 1;
+        $this->setTenantEmail('tenant11@example.com');
+
+        $client = $this->getClient();
+        $repo = $this->getEntityRepository('RjDataBundle:Payment');
+        $result = $repo->findOneByIdForUser($id, $this->getTenant()->getUsername());
+
+        $client->request(
+            'GET',
+            self::URL_PREFIX . "/payments/" . $this->getIdEncoder()->encode($id),
+            [],
+            [],
+            [
+                'HTTP_AUTHORIZATION' => 'Bearer ' . static::TENANT_ACCESS_TOKEN,
+            ]
+        );
+        $answer = $this->parseContent($client->getResponse()->getContent());
+
+        $this->assertResponse($client->getResponse());
+        $this->assertEquals($result->getType(), $answer["type"]);
+        $this->assertEquals($result->getAmount(), $answer["rent"]);
+        $this->assertEquals("0.00", $answer["other"]);
+        $this->assertEquals($result->getDueDate(), $answer["day"]);
+        $this->assertEquals($result->getStartMonth(), $answer["month"]);
+        $this->assertEquals($result->getStartYear(), $answer["year"]);
+        $this->assertEquals($result->getEndMonth(), $answer["end_month"]);
+        $this->assertEquals($result->getEndYear(), $answer["end_year"]);
+        $this->assertEquals(date("Y-m", $result->getPaidFor()->getTimestamp()), $answer["paid_for"]);
+        $this->assertEquals($result->getStatus(), $answer["status"]);
+        $this->assertEquals($result->getContract()->getId(), $answer["contract_url"]);
+        $this->assertEquals($result->getPaymentAccountId(), $answer["payment_account_url"]);
+    }
+
+}
