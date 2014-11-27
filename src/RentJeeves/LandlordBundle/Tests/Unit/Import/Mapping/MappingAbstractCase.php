461e6e50fe331ad65a161afa96428936b8edbe2d
Cary Penniman <cary@renttrack.com>
2014-11-26 17:33:25 -0800

    Merged unit tests that test same class

--- src/RentJeeves/LandlordBundle/Tests/Unit/Import/Mapping/MappingAbstractCase.php
+++ src/RentJeeves/LandlordBundle/Tests/Unit/Import/Mapping/MappingAbstractCase.php
@@ -2,7 +2,7 @@
 namespace RentJeeves\LandlordBundle\Tests\Unit\Import\Mapping;
 
 use RentJeeves\LandlordBundle\Accounting\Import\Mapping\MappingAbstract;
-
+use \DateTime;
 
 class MappingTest extends MappingAbstract
 {
@@ -40,6 +40,53 @@ class MappingTest extends MappingAbstract
 
 class MappingAbstractCase extends \PHPUnit_Framework_TestCase
 {
+    protected static $countDateFormat = 0;
+
+    public function dataProviderForCheckDateFormat()
+    {
+        return array(
+            array('02/04/2014', '2014-02-04', 'm/d/Y'),
+            array('2014-02-04', '2014-02-04', 'Y-m-d'),
+            array('20140204', '2014-02-04', 'Ymd'),
+            array('02042014', '2014-02-04', 'dmY'),
+            array('2014-02-04', '2014-02-04', 'mdY'),
+            array('2014/02/04', '2014-02-04', 'Y/m/d'),
+            array('4/2/14', '2014-02-04','j/n/y'),
+            array('04/02/14', '2014-02-04', 'd/m/y'),
+            array('02/04/14', '2014-02-04', 'm/d/y'),
+            array('02-04-14', '2014-02-04', 'm-d-y'),
+            array('02-04-2014', '2014-02-04', 'm-d-Y'),
+            array('2-4-2014', '2014-02-04', 'n-j-Y'),
+            array('2-4-14', '2014-02-04', 'n-j-y'),
+            array('April 2, 2014', '2014-02-04', 'F d, Y'),
+            array('04-Apr-14', '2014-02-04', 'd-M-y'),
+        );
+    }
+
+    /**
+     * This test check ImportMapping::$mappingDates for make sure all format correct
+     *
+     * @test
+     * @dataProvider dataProviderForCheckDateFormat
+     */
+    public function checkDateFormat($dateStringForParse, $dateStringForCheck, $format)
+    {
+        static::$countDateFormat++;
+        $date = DateTime::createFromFormat($dateStringForParse, $format);
+    }
+
+    /**
+     * @test
+     * @depends checkDateFormat
+     */
+    public function checkCountDateFormat()
+    {
+        $this->assertEquals(
+            count(MappingAbstract::$mappingDates),
+            static::$countDateFormat,
+            'Some date format not check, please add it into data dataProviderForDateFormat'
+        );
+    }
 
     public function streetDataProvider()
     {
