1bd56bc743addf033acc0dc0e33ee51e6db7166f
Yuriy Borovchuk <yurez.b@gmail.com>
2014-12-22 16:08:12 +0200

    RT-932: remove useless comments

--- app/DoctrineMigrations/Version20141220160110.php
+++ app/DoctrineMigrations/Version20141220160110.php
@@ -5,14 +5,10 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20141220160110 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             $this->connection->getDatabasePlatform()->getName() != "mysql",
             "Migration can only be executed safely on 'mysql'."
@@ -34,7 +30,6 @@ class Version20141220160110 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             $this->connection->getDatabasePlatform()->getName() != "mysql",
             "Migration can only be executed safely on 'mysql'."
