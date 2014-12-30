59e636edbf208f870f8b883f91f3452c84cabf70
Yuriy Borovchuk <yurez.b@gmail.com>
2014-12-30 17:16:19 +0200

    RT-1004: refactoring code Business Date Calc

--- src/RentJeeves/CheckoutBundle/Payment/BusinessDaysCalculator.php
+++ src/RentJeeves/CheckoutBundle/Payment/BusinessDaysCalculator.php
@@ -10,54 +10,6 @@ use DateTime;
  */
 class BusinessDaysCalculator
 {
-    protected $ccBusinessDays;
-    protected $achBusinessDays;
-
-    /**
-     * @DI\InjectParams({
-     *     "ccBusinessDays" = @DI\Inject("%business_days_cc%"),
-     *     "achBusinessDays" = @DI\Inject("%business_days_ach%"),
-     * })
-     */
-    public function __construct($ccBusinessDays, $achBusinessDays)
-    {
-        $this->ccBusinessDays = $ccBusinessDays;
-        $this->achBusinessDays = $achBusinessDays;
-    }
-
-    public function getBusinessDate(DateTime $startDate, $targetShift)
-    {
-        $businessDate = $startDate;
-        $shiftedDays = 0;
-        $targetShift += 1;
-
-        while ($shiftedDays <= $targetShift) {
-            $currentDayOfWeek = $businessDate->format('N');
-
-            if ($currentDayOfWeek <= 5) {
-                $shiftedDays++;
-            }
-
-            if ($shiftedDays == $targetShift) {
-                break;
-            }
-
-            $businessDate = $businessDate->modify('+1 day');
-        }
-
-        return $businessDate;
-    }
-
-    public function getCreditCardBusinessDate(DateTime $startDate)
-    {
-        return $this->getBusinessDate($startDate, $this->ccBusinessDays);
-    }
-
-    public function getACHBusinessDate(DateTime $startDate)
-    {
-        return $this->getBusinessDate($startDate, $this->achBusinessDays);
-    }
-
     public function getNextBusinessDate(DateTime $currentDate)
     {
         switch ($currentDate->format('N')) {
--- src/RentJeeves/CheckoutBundle/Tests/Unit/BusinessDaysCalculatorCase.php
+++ src/RentJeeves/CheckoutBundle/Tests/Unit/BusinessDaysCalculatorCase.php
@@ -10,102 +10,15 @@ class BusinessDaysCalculatorCase extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
-     */
-    public function shouldBeConstructedWithCCAndACHBusinessDays()
-    {
-        $rc = new ReflectionClass('RentJeeves\CheckoutBundle\Payment\BusinessDaysCalculator');
-        $instance = $rc->newInstanceArgs(array('1', '3'));
-
-        $ccField = $rc->getProperty('ccBusinessDays');
-        $ccField->setAccessible(true);
-        $achField = $rc->getProperty('achBusinessDays');
-        $achField->setAccessible(true);
-
-        $this->assertEquals(1, $ccField->getValue($instance));
-        $this->assertEquals(3, $achField->getValue($instance));
-    }
-
-    /**
-     * @test
-     * @dataProvider provideCreditCardsShiftParameters
-     */
-    public function shouldCalcCreditCardBusinessDays($startDate, $shiftCc, $shiftAch, $expectedBusinessDate)
-    {
-        $calc = new BusinessDaysCalculator($shiftCc, $shiftAch);
-
-        $result = $calc->getCreditCardBusinessDate($startDate);
-
-        $this->assertEquals($expectedBusinessDate, $result);
-    }
-
-    /**
-     * @test
-     * @dataProvider provideACHShiftParameters
-     */
-    public function shouldCalcACHBusinessDays($startDate, $shiftCc, $shiftAch, $expectedBusinessDate)
-    {
-        $calc = new BusinessDaysCalculator($shiftCc, $shiftAch);
-
-        $result = $calc->getACHBusinessDate($startDate);
-
-        $this->assertEquals($expectedBusinessDate, $result);
-    }
-
-    /**
-     * @test
-     * @dataProvider provideRandomShiftParameters
-     */
-    public function shouldCalcBusinessDateWithRandomShift($startDate, $shift, $expectedBusinessDate)
-    {
-        $calc = new BusinessDaysCalculator(1, 3);
-
-        $result = $calc->getBusinessDate($startDate, $shift);
-
-        $this->assertEquals($expectedBusinessDate, $result);
-    }
-
-    /**
-     * @test
      * @dataProvider provideNextParameters
      */
     public function shouldCalcNextBusinessDate($startDate, $expectedBusinessDate)
     {
-        $calc = new BusinessDaysCalculator(0, 0);
+        $calc = new BusinessDaysCalculator();
 
         $result = $calc->getNextBusinessDate($startDate);
 
         $this->assertEquals($expectedBusinessDate, $result);
-    }
-
-    public function provideCreditCardsShiftParameters()
-    {
-        return array(
-            array(new DateTime("2014-02-07"), 1, 3, new DateTime('2014-02-10')),
-            array(new DateTime("2014-02-10"), 1, 3, new DateTime('2014-02-11')),
-            array(new DateTime("2014-02-09"), 1, 3, new DateTime('2014-02-11')),
-            array(new DateTime("2014-02-08"), 1, 3, new DateTime('2014-02-11')),
-        );
-    }
-
-    public function provideACHShiftParameters()
-    {
-        return array(
-            array(new DateTime("2014-02-07"), 1, 3, new DateTime('2014-02-12')),
-            array(new DateTime("2014-02-10"), 1, 3, new DateTime('2014-02-13')),
-            array(new DateTime("2014-02-09"), 1, 3, new DateTime('2014-02-13')),
-            array(new DateTime("2014-02-08"), 1, 3, new DateTime('2014-02-13')),
-        );
-    }
-
-    public function provideRandomShiftParameters()
-    {
-        return array(
-            array(new DateTime("2014-02-07"), 1, new DateTime('2014-02-10')),
-            array(new DateTime("2014-02-10"), 2, new DateTime('2014-02-12')),
-            array(new DateTime("2014-02-09"), 3, new DateTime('2014-02-13')),
-            array(new DateTime("2014-02-08"), 4, new DateTime('2014-02-14')),
-            array(new DateTime("2014-02-08"), 5, new DateTime('2014-02-17')),
-        );
     }
 
     public function provideNextParameters()
