b6b491d0e3b69f8579d467cc235d8616b7d0fdf9
Yuriy Borovchuk <yurez.b@gmail.com>
2015-01-16 15:28:07 +0200

    RT-1020: Change template for Due Date Email

--- src/RentJeeves/CoreBundle/Mailer/Mailer.php
+++ src/RentJeeves/CoreBundle/Mailer/Mailer.php
@@ -99,14 +99,15 @@ class Mailer extends BaseMailer
         return $this->sendBaseLetter($sTemplate, $vars, $tenant->getEmail(), $tenant->getCulture());
     }
 
-    public function sendRjPaymentDue($tenant, $holding, $contract, $recurring = false, $sTemplate = 'rjPaymentDue')
+    public function sendRjPaymentDue($tenant, $holding, $contract, $paymentType = null, $sTemplate = 'rjPaymentDue')
     {
-        $vars = array(
+        $vars = [
             'nameHolding' => $holding->getName(),
             'nameTenant' => $tenant->getFullName(),
             'address' => $contract->getRentAddress($contract->getProperty(), $contract->getUnit()),
-            'recurring' => $recurring,
-        );
+            'paymentType' => $paymentType,
+        ];
+
         return $this->sendBaseLetter($sTemplate, $vars, $tenant->getEmail(), $tenant->getCulture());
     }
 
