8dd7821d231196e3419787e65b51acf9c41ac111
Yuriy Borovchuk <yurez.b@gmail.com>
2015-01-21 13:26:56 +0200

    RT-1028: Change "Due On" displaying on Tenant Dashboard

---
+++ src/RentJeeves/CoreBundle/Tests/Unit/TwigExtensionCase.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RentJeeves\CoreBundle\Tests\Unit;
+
+use RentJeeves\CoreBundle\Twig\Extension;
+use RentJeeves\TestBundle\Functional\BaseTestCase;
+
+class TwigExtensionCase extends BaseTestCase
+{
+    /**
+     * @var Extension
+     */
+    protected $twigExtension;
+
+    public function setUp()
+    {
+        $this->twigExtension = new Extension();
+    }
+
+    public static function ordinalNumbersDataProvider()
+    {
+        return [
+            [1, '1st'],
+            [2, '2nd'],
+            [3, '3rd'],
+            [5, '5th'],
+            ['N?A', 'N?A'],
+            [20, '20th'],
+            [12, '12th'],
+            [15, '15th'],
+            [21, '21st'],
+            [32, '32nd'],
+            [113, '113th'],
+            [53, '53rd'],
+            [66, '66th'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider ordinalNumbersDataProvider
+     */
+    public function ordinalNumber($number, $result)
+    {
+        $ordinalNumber = $this->twigExtension->ordinalNumber($number);
+
+        $this->assertEquals($result, $ordinalNumber);
+
+        return;
+    }
+}
--- src/RentJeeves/CoreBundle/Twig/Extension.php
+++ src/RentJeeves/CoreBundle/Twig/Extension.php
@@ -14,8 +14,9 @@ class Extension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('array_values', array($this, 'arrayValues')),
-            new \Twig_SimpleFilter('phone_number', array($this, 'phoneNumber')),
+            new \Twig_SimpleFilter('array_values', [$this, 'arrayValues']),
+            new \Twig_SimpleFilter('phone_number', [$this, 'phoneNumber']),
+            new \Twig_SimpleFilter('ordinal_number', [$this, 'ordinalNumber']),
         );
     }
 
@@ -29,6 +30,24 @@ class Extension extends \Twig_Extension
         $phone = preg_replace('/\D/', '', $phone);
 
         return ($phone) ? substr($phone, 0, 3) . '-' . substr($phone, 3, 3) . '-' . substr($phone, 6, 4) : '';
+    }
+
+    public function ordinalNumber($number)
+    {
+        if (!is_numeric($number)) {
+            return $number;
+        }
+        // Special case "teenth"
+        if (($number / 10) % 10 != 1) {
+            // Handle 1st, 2nd, 3rd
+            switch ($number % 10) {
+                case 1: return $number . 'st';
+                case 2: return $number . 'nd';
+                case 3: return $number . 'rd';
+            }
+        }
+        // Everything else is "nth"
+        return $number . 'th';
     }
 
     public function getName()
