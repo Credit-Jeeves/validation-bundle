f3b5b58bf49388dc01540311acb20cb0fdf8f736
Cary Penniman <cary@renttrack.com>
2015-01-27 00:29:29 +0000

    renaming enums using migration needs both old and new types defined

--- src/RentJeeves/DataBundle/RjDataBundle.php
+++ src/RentJeeves/DataBundle/RjDataBundle.php
@@ -12,7 +12,7 @@ class RjDataBundle extends Bundle
      * @var bool
      */
     protected static $isLoaded = false;
-    
+
     /**
      * {@inheritdoc}
      */
@@ -32,6 +32,7 @@ class RjDataBundle extends Bundle
             Type::addType('PaymentTypeACH', 'RentJeeves\DataBundle\Enum\PaymentTypeACH');
             Type::addType('PaymentTypeCC', 'RentJeeves\DataBundle\Enum\PaymentTypeCC');
             Type::addType('ExternalApi', 'RentJeeves\DataBundle\Enum\ExternalApi');
+            Type::addType('YardiPaymentAccepted', 'RentJeeves\DataBundle\Enum\PaymentAccepted'); #TODO: CKP: needed for column rename migration, remove me after release 4.3
             Type::addType('PaymentAccepted', 'RentJeeves\DataBundle\Enum\PaymentAccepted');
             Type::addType('ApiIntegrationType', 'RentJeeves\DataBundle\Enum\ApiIntegrationType');
 
