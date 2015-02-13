e538c6251d058fe1a0ad03dfce00bc56fa7bedbe
Darryl Eaton <darryl@creditjeeves.com>
2015-02-13 15:24:21 -0800

    fixed unresoved merge conflict

--- src/RentJeeves/ComponentBundle/Service/ResidentManager.php
+++ src/RentJeeves/ComponentBundle/Service/ResidentManager.php
@@ -71,51 +71,42 @@ class ResidentManager
          * @var $errorsResidentMapping ConstraintViolationList
          */
         $errorsResidentMapping = $this->validator->validate($residentMapping, ['unique_entity']);
-<<<<<<< HEAD
-        $existingMapping = $this->getExistingMapping($residentMapping);
-        if ($errorsResidentMapping->count() === 1 &&
-            ($existingMapping->getTenant()->getEmail() !== $residentMapping->getTenant()->getEmail())
-        ) {
-=======
         $existingMappingTenant = $this->getExistingMappingByTenant($residentMapping);
         $existingMappingResident = $this->getExistingMappingByResidentId($residentMapping);
 
         if ($errorsResidentMapping->count() === 1) {
 
->>>>>>> master
             $errors[] = $this->translator->trans(
                 $errorsResidentMapping->get(0)->getMessage(),
                 array(
                     '%support_email%'   => $this->supportEmail,
-<<<<<<< HEAD
-                    '%email%'           => $existingMapping->getTenant()->getEmail()
-=======
                     '%email%'           => $existingMappingResident->getTenant()->getEmail()
->>>>>>> master
                 )
             );
         }
 
-<<<<<<< HEAD
-
-
-        if (empty($errors)) {
-            $this->clearWaitingRoom($landlord, $residentMapping);
-            $this->em->persist($residentMapping);
-        }
-=======
         //Create action
         if (empty($errors) && empty($existingMappingTenant)) {
             $this->clearWaitingRoom($landlord, $residentMapping);
             $this->em->persist($residentMapping);
+
+            return [];
         }
+
         //Update action
-        if (empty($error) && !empty($existingMappingTenant)) {
+        if (empty($errors) && !empty($existingMappingTenant) ||
+            (
+                !empty($errors) &&
+                !empty($existingMappingResident) && !empty($existingMappingTenant) &&
+                ($existingMappingResident->getTenant()->getEmail() === $existingMappingTenant->getTenant()->getEmail())
+            )
+        ) {
             $existingMappingTenant->setResidentId($residentMapping->getResidentId());
             $this->em->persist($existingMappingTenant);
             unset($residentMapping);
+
+            return [];
         }
->>>>>>> master
 
         return $errors;
     }
@@ -163,9 +154,6 @@ class ResidentManager
      * @param ResidentMapping $residentMapping
      * @return ResidentMapping
      */
-<<<<<<< HEAD
-    protected function getExistingMapping(ResidentMapping $residentMapping)
-=======
     protected function getExistingMappingByTenant(ResidentMapping $residentMapping)
     {
         /**
@@ -187,23 +175,18 @@ class ResidentManager
      * @return ResidentMapping
      */
     protected function getExistingMappingByResidentId(ResidentMapping $residentMapping)
->>>>>>> master
     {
         /**
          * @var $residentMapping ResidentMapping
          */
         $residentMapping = $this->em->getRepository('RjDataBundle:ResidentMapping')->findOneBy(
             array(
-<<<<<<< HEAD
-                'holding' => $residentMapping->getHolding()->getId(),
-                'residentId' => $residentMapping->getResidentId(),
-=======
                 'holding'        => $residentMapping->getHolding()->getId(),
                 'residentId'     => $residentMapping->getResidentId()
->>>>>>> master
             )
         );
 
         return $residentMapping;
     }
 }
+
