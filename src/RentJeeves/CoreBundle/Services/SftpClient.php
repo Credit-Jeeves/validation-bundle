ad071ac32139e416fe0351cd0a3afee04b5dc273
onatskyy <onatskyy@gmail.com>
2015-04-03 15:44:00 +0300

    RT-1249: added downloader, interface, unit tests and SftpClient class

---
+++ src/RentJeeves/CoreBundle/Services/SftpClient.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace RentJeeves\CoreBundle\Services;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * This class is added to be able to write UNIT tests.
+ * Because to write tests for resources is impossible.
+ *
+ * @Di\Service("sftp_client", public=false)
+ */
+class SftpClient
+{
+    /**
+     * @var resource
+     */
+    private $ssh;
+
+    /**
+     * @var resource
+     */
+    private $sftp;
+
+    /**
+     * @param string $host
+     * @param int $port
+     */
+    public function sshConnect($host, $port)
+    {
+        if (false === is_int($port)) {
+            throw new \LogicException('Parameter "port" must be an integer');
+        }
+        $this->ssh = ssh2_connect($host, $port);
+    }
+
+    /**
+     * @param $login
+     * @param $password
+     */
+    public function sshAuthPassword($login, $password)
+    {
+        if (false === is_resource($this->ssh)) {
+            throw new \LogicException('SSH connection is not established. Use "sshConnect" for create it.');
+        }
+        ssh2_auth_password($this->ssh, $login, $password);
+    }
+
+    public function sshSftp()
+    {
+        if (false === is_resource($this->ssh)) {
+            throw new \LogicException('SSH connection is not established. Use "sshConnect" for create it.');
+        }
+        $this->sftp = ssh2_sftp($this->ssh);
+    }
+
+    /**
+     * @return resource
+     */
+    public function getSsh()
+    {
+        return $this->ssh;
+    }
+
+    /**
+     * @return resource
+     */
+    public function getSftp()
+    {
+        return $this->sftp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSftpPath()
+    {
+        if (false === is_resource($this->ssh)) {
+            throw new \LogicException('SFTP subsystem is not initialized. Use "sshSftp" for initialize it.');
+        }
+
+        return 'ssh2.sftp://' . $this->getSftp();
+    }
+}
