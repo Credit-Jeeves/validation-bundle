fe648c487d3727258e5b7311350a35aefbae2705
caryp <caryp@users.noreply.github.com>
2015-04-08 12:55:43 -0700

    Care about major revision changes in Yardi API
    This avoids changing test every time they bump the minor version

--- src/RentJeeves/ExternalApiBundle/Tests/Command/YardiVersionNumberCommandCase.php
+++ src/RentJeeves/ExternalApiBundle/Tests/Command/YardiVersionNumberCommandCase.php
@@ -27,7 +27,7 @@ class YardiVersionNumberCommandCase extends BaseTestCase
         );
 
         $this->assertRegExp(
-            '/Current version for holding Rent Holding is 7Sv4\..*/',
+            '/Current version for holding Rent Holding is 7Sv.*/',
             trim($commandTester->getDisplay())
         );
     }
