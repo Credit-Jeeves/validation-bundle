ba4e7628b137455c67dd05a3bde621ecd8d4fbc8
Yuriy Borovchuk <yurez.b@gmail.com>
2015-04-17 10:55:37 +0300

    RT-1175: fixed code style and one issues with Heartlands Entity

--- src/RentJeeves/CheckoutBundle/PaymentProcessor/Exception/PaymentProcessorInvalidArgumentException.php
+++ src/RentJeeves/CheckoutBundle/PaymentProcessor/Exception/PaymentProcessorInvalidArgumentException.php
@@ -6,8 +6,11 @@ class PaymentProcessorInvalidArgumentException extends \InvalidArgumentException
 {
     public static function createInvalidPaymentProcessor($paymentProcessor)
     {
-        return new self(sprintf('Invalid Payment Processor Type. It should be %s',
-            $paymentProcessor
-        ));
+        return new self(
+            sprintf(
+                'Invalid Payment Processor Type. It should be %s',
+                $paymentProcessor
+            )
+        );
     }
 }
