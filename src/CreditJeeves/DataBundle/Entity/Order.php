4a9eb051e4a033f00d2832c6dd54d7785fb57b01
Darryl Eaton <darryl@renttrack.com>
2015-05-28 00:06:01 +0000

    RT-1408 MRI Push payment types wrong and dont send check number for credit card

--- src/CreditJeeves/DataBundle/Entity/Order.php
+++ src/CreditJeeves/DataBundle/Entity/Order.php
@@ -1003,11 +1003,11 @@ class Order extends BaseOrder
     public function getMriPaymentType()
     {
         if ($this->getType() === OrderType::HEARTLAND_CARD) {
-            return 'C';
+            return 'K';
         }
 
         if ($this->getType() === OrderType::HEARTLAND_BANK) {
-            return 'K';
+            return 'C';
         }
 
         return null;
@@ -1038,7 +1038,11 @@ class Order extends BaseOrder
      */
     public function getMriCheckNumber()
     {
-        return $this->getCompleteTransaction()->getTransactionId();
+        if ($this->getType() === OrderType::HEARTLAND_BANK) {
+            return $this->getCompleteTransaction()->getTransactionId();
+        }
+
+        return null;
     }
 
     /**
