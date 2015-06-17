52f4b71a78ec450c1eb247c19dd5159c05681669
Tobur <sharamko11@yandex.ru>
2015-06-17 16:06:47 +0300

    RT-855 fix wrong merge

--- src/RentJeeves/AdminBundle/Controller/CoreController.php
+++ src/RentJeeves/AdminBundle/Controller/CoreController.php
@@ -2,15 +2,11 @@
 
 namespace RentJeeves\AdminBundle\Controller;
 
-use RentJeeves\CoreBundle\Report\RentalReport;
-use RentJeeves\CoreBundle\Report\RentalReportData;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sonata\AdminBundle\Controller\CoreController as BaseController;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class CoreController extends BaseController
 {
@@ -31,48 +27,5 @@ class CoreController extends BaseController
         $request->getSession()->set('property_id', null);
 
         return parent::dashboardAction();
-    }
-
-    /**
-     * @Route("report", name="sonata_admin_rental_report")
-     * @Template()
-     *
-     * @return array
-     */
-    public function reportAction(Request $request)
-    {
-        $rentalReportData = new RentalReportData();
-        $reportType = $this->createForm('rental_report', $rentalReportData);
-        $reportType->handleRequest($request);
-
-        if ($reportType->isValid()) {
-            /** @var RentalReport $report */
-            $report = $this->get('rental_report.factory')->getReport($rentalReportData);
-            $report->build($rentalReportData);
-
-            if ($report->isEmpty()) {
-                $this->get('session')->getFlashBag()->add(
-                    'notice',
-                    $this->get('translator')->trans(
-                        'admin.report.notice',
-                        ['%m%' => $rentalReportData->getMonth()->format('m/Y')]
-                    )
-                );
-            } else {
-                $result = $this->get('jms_serializer')->serialize($report, $report->getSerializationType());
-                $response = new Response($result, 200);
-                $attachment = $response->headers->makeDisposition(
-                    ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-                    $report->getReportFilename()
-                );
-                $response->headers->set('Content-Disposition', $attachment);
-
-                return $response;
-            }
-        }
-
-        return [
-            'rentalReport' => $reportType->createView(),
-        ];
     }
 }
