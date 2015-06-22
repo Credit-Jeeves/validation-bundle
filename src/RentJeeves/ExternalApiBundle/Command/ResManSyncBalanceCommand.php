a9ff326cf23eb8095b30946af64c5774189f04ff
Tobur <sharamko11@yandex.ru>
2015-06-22 16:29:15 +0300

    RT-1344 add command

---
+++ src/RentJeeves/ExternalApiBundle/Command/ResManSyncBalanceCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RentJeeves\ExternalApiBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResManSyncBalanceCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('api:resman:sync-balance')
+            ->setDescription('Update resident balances.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getContainer()
+            ->get('resman.resident_balance_sync')
+            ->usingOutput($output)
+            ->run();
+    }
+}
