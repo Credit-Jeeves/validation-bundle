7d256183ae016190d98fa43da42d2fd32fc628e9
Tobur <sharamko11@yandex.ru>
2015-07-05 13:16:14 +0300

    RT-1468: implemented test and functional when we are working with tenant status on the import

--- src/RentJeeves/LandlordBundle/Tests/Unit/Import/Handler/HandlerAbstractCase.php
+++ src/RentJeeves/LandlordBundle/Tests/Unit/Import/Handler/HandlerAbstractCase.php
@@ -1,6 +1,7 @@
 <?php
 namespace RentJeeves\LandlordBundle\Tests\Unit\Import\Handler;
 
+use CreditJeeves\DataBundle\Entity\Group;
 use RentJeeves\LandlordBundle\Accounting\Import\Mapping\MappingAbstract;
 use RentJeeves\LandlordBundle\Accounting\Import\Storage\StorageCsv;
 use RentJeeves\LandlordBundle\Model\Import;
@@ -23,6 +24,11 @@ class HandlerAbstractCase extends BaseTestCase
         $currentImportModel = $handlerTestReflection->getProperty('translator');
         $currentImportModel->setAccessible(true);
         $currentImportModel->setValue($handler, $this->getContainer()->get('translator'));
+
+        $currentImportModel = $handlerTestReflection->getProperty('translator');
+        $currentImportModel->setAccessible(true);
+        $currentImportModel->setValue($handler, $this->getContainer()->get('translator'));
+
         /** @var StorageCsv $storageCsv */
         $storageCsv = $this->getContainer()->get('accounting.import.storage.csv');
         $storageCsv->setDateFormat('Y-m-d');
@@ -71,5 +77,26 @@ class HandlerAbstractCase extends BaseTestCase
         $this->assertNotNull($import->getMoveOut());
         $this->assertEquals($moveOut->format('ymd'), $import->getMoveOut()->format('ymd'));
         $this->assertEquals(false, $import->isSkipped());
+        /** @var Group $groupModel */
+        $groupModel = $this->getEntityManager()->getRepository('DataBundle:Group')->findOneByName('Test Rent Group');
+        $this->assertNotEmpty($groupModel);
+        $this->assertTrue($groupModel->getHolding()->isAllowedFutureContract());
+        $groupReflection = $handlerTestReflection->getProperty('group');
+        $groupReflection->setAccessible(true);
+        $groupReflection->setValue($handler, $groupModel);
+
+        $row = [
+            MappingAbstract::KEY_TENANT_STATUS => 'f',
+            MappingAbstract::KEY_MOVE_OUT => '',
+        ];
+        $import->setMoveOut(null);
+        $import->setIsSkipped(false);
+        $checkTenantStatus->invoke($handler, $row);
+        $this->assertNull($import->getMoveOut());
+        $this->assertEquals(false, $import->isSkipped());
+
+        $groupModel->getHolding()->setIsAllowedFutureContract(false);
+        $checkTenantStatus->invoke($handler, $row);
+        $this->assertEquals(true, $import->isSkipped());
     }
 }
