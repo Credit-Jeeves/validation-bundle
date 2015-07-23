ba199882b48606a7b8cba8289f7c5700c303cd0f
onatskyy <onatskyy@gmail.com>
2015-07-23 18:26:09 +0300

    RT-1537: remove extra field from cj_order

---
+++ app/DoctrineMigrations/Version20150723182127.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20150723182127 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() != "mysql",
+            "Migration can only be executed safely on 'mysql'."
+        );
+
+        $this->addSql(
+            "ALTER TABLE cj_order
+                DROP orderType"
+        );
+    }
+
+    public function down(Schema $schema)
+    {
+    }
+}
