69ba81a3e30acf95f28dbd3f4b50e7fb6c25ffb4
onatskyy <onatskyy@gmail.com>
2015-07-23 18:39:03 +0300

    remove coments

--- app/DoctrineMigrations/Version20150723183609.php
+++ app/DoctrineMigrations/Version20150723183609.php
@@ -5,9 +5,6 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20150723183609 extends AbstractMigration
 {
     public function up(Schema $schema)
@@ -33,7 +30,5 @@ class Version20150723183609 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
     }
 }
