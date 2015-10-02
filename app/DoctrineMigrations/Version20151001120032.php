daf41e4d612d066929aeb59ef7b756db136cd122
Tobur <sharamko11@yandex.ru>
2015-10-02 18:14:25 +0300

    RT-1689 added trans message

--- app/DoctrineMigrations/Version20151001120032.php
+++ app/DoctrineMigrations/Version20151001120032.php
@@ -17,7 +17,7 @@ class Version20151001120032 extends AbstractMigration
             "ALTER TABLE rj_group_settings
                 ADD allowed_debit_fee TINYINT(1) DEFAULT '0' NOT NULL,
                 ADD type_debit_fee ENUM('flat_fee','percentage')
-                    COMMENT '(DC2Type:TypeDebitFees)' DEFAULT 'percentage' NOT NULL,
+                    COMMENT '(DC2Type:TypeDebitFee)' DEFAULT 'percentage' NOT NULL,
                 ADD debit_fee NUMERIC(10,
                 2) DEFAULT NULL"
         );
