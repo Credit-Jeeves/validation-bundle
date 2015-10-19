7109441161d6bc75718160f22e198072252d7f1a
onatskyy <onatskyy@gmail.com>
2015-10-19 13:25:22 +0300

    Revert rename constant

--- src/RentJeeves/DataBundle/Enum/PaymentAccountType.php
+++ src/RentJeeves/DataBundle/Enum/PaymentAccountType.php
@@ -6,6 +6,6 @@ use CreditJeeves\CoreBundle\Enum;
 class PaymentAccountType extends Enum
 {
     const BANK = 'bank';
-    const CREDIT_CARD = 'card';
+    const CARD = 'card';
     const DEBIT_CARD = 'debit_card';
 }
