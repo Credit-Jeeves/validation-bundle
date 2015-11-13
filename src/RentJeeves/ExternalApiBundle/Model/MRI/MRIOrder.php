9fe3a01a914e74b6834dd8e6150904b03d455c82
onatskyy <onatskyy@gmail.com>
2015-11-13 12:22:24 +0200

    RT-1775: fixed description

--- src/RentJeeves/ExternalApiBundle/Model/MRI/MRIOrder.php
+++ src/RentJeeves/ExternalApiBundle/Model/MRI/MRIOrder.php
@@ -185,7 +185,7 @@ class MRIOrder
     public function getMriDescription()
     {
         return sprintf(
-            'RT Payment T#%d B#%s',
+            'RT Payment T#%s B#%s',
             $this->order->getCompleteTransaction()->getBatchId(),
             $this->order->getCompleteTransaction()->getId()
         );
