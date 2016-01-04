be62f5793fdc8a3cfea392d34d103fba85100af0
onatskyy <onatskyy@gmail.com>
2016-01-04 14:59:43 +0200

    fixed error after rebase

--- src/RentJeeves/TestBundle/DependencyInjection/RjTestExtension.php
+++ src/RentJeeves/TestBundle/DependencyInjection/RjTestExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class RjImportExtension extends Extension
+class RjTestExtension extends Extension
 {
     /**
      * {@inheritDoc}
