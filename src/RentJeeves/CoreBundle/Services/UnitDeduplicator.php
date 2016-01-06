d9d348737b0ed02ddd3f2d6a9de634e29af6f2c6
onatskyy <onatskyy@gmail.com>
2016-01-06 15:03:43 +0200

    fix problem with is_single field (null)

--- src/RentJeeves/CoreBundle/Services/UnitDeduplicator.php
+++ src/RentJeeves/CoreBundle/Services/UnitDeduplicator.php
@@ -113,13 +113,13 @@ class UnitDeduplicator
         $this->updateContractsWaitingForUnit($sourceUnit, $destinationUnit);
         $this->updateContractsForUnit($sourceUnit, $destinationUnit);
         if (false === $this->dryRunMode) {
-            if (false === $isSingleSourceProperty) { // not remove units from isSingle Property
+            if (true !== $isSingleSourceProperty) { // not remove units from isSingle Property
                 $this->disableSoftDelete();
                 $this->em->remove($sourceUnit);
             }
             $this->em->flush();
             $this->logger->info(
-                $isSingleSourceProperty ? 'SrcUnit is deduplicated, but not deleted.' : 'SrcUnit is deleted.'
+                true !== $isSingleSourceProperty ? 'SrcUnit is deduplicated, but not deleted.' : 'SrcUnit is deleted.'
             );
         }
 
