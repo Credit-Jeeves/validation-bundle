5b67a67d746a9ad14b9a68aa5c70f7762f1761d6
onatskyy <onatskyy@gmail.com>
2016-01-20 14:30:25 +0200

    RT-2047: Created new table, added migration

---
+++ src/RentJeeves/DataBundle/Entity/ProfitStarsRegisteredContracts.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RentJeeves\DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use RentJeeves\DataBundle\Model\ProfitStarsRegisteredContracts as ProfitStarsRegisteredContractsModel;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="rj_profitstars_registered_contracts",
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="unique_index_constraint", columns={"contract_id", "location_id"})
+ *     }
+ * )
+ */
+class ProfitStarsRegisteredContracts extends ProfitStarsRegisteredContractsModel
+{
+
+}
