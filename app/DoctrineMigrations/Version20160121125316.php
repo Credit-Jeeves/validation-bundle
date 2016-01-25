28bdf09e5ef3929547db8b1d7a38538897167ecd
Alexandr Sharamko <sharamko11@yandex.ru>
2016-01-25 12:08:12 +0200

    RT-2054 fixed migration for new accounting enum

--- app/DoctrineMigrations/Version20160121125316.php
+++ app/DoctrineMigrations/Version20160121125316.php
@@ -20,11 +20,17 @@ class Version20160121125316 extends AbstractMigration
             enum('none','yardi voyager','resman','mri','amsi', 'boston post')
             COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:AccountingSystem)'"
         );
+
+        $this->addSql(
+            "ALTER TABLE rj_payment_batch_mapping
+            CHANGE COLUMN `accounting_package_type` `accounting_package_type`
+            enum('none','yardi voyager','resman','mri','amsi', 'boston post')
+            COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:AccountingSystem)'"
+        );
     }
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             $this->connection->getDatabasePlatform()->getName() != "mysql",
             "Migration can only be executed safely on 'mysql'."
@@ -33,6 +39,13 @@ class Version20160121125316 extends AbstractMigration
         $this->addSql(
             "ALTER TABLE cj_holding
             CHANGE COLUMN  `accounting_system` `api_integration_type`
+            enum('none','yardi voyager','resman','mri','amsi')
+            COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:ApiIntegrationType)'"
+        );
+
+        $this->addSql(
+            "ALTER TABLE rj_payment_batch_mapping
+            CHANGE COLUMN `accounting_package_type` `accounting_package_type`
             enum('none','yardi voyager','resman','mri','amsi')
             COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:ApiIntegrationType)'"
         );
