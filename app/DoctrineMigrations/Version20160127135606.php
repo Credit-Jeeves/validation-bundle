cb7721599835b0219cafaaaa7e2d034abb7536cd
onatskyy <onatskyy@gmail.com>
2016-01-31 14:40:04 +0200

    fix migration

--- app/DoctrineMigrations/Version20160127135606.php
+++ app/DoctrineMigrations/Version20160127135606.php
@@ -10,7 +10,7 @@ class Version20160127135606 extends AbstractMigration
     public function up(Schema $schema)
     {
         $template = <<<tmplt
-        {% extends 'RjComponentBundle:Mailer:base.html.twig' %}
+        {% extends "RjComponentBundle:Mailer:base.html.twig" %}
         {% block h1 %}Rent Payment Initiated{% endblock %}
         {% block email %}
             Hi {{ tenantName }}! <br /><br />
@@ -19,55 +19,55 @@ class Version20160127135606 extends AbstractMigration
             <br /><br />
             The details:
 
-            <table width='100%' style='border: 1px solid #C8C8C8; border-collapse: collapse;'>
+            <table width="100%" style="border: 1px solid #C8C8C8; border-collapse: collapse;">
             <tbody>
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'common.charged_at' | trans }}:</td>
-                <td style='padding:5px;'>{{ orderTime }}</td>
-            </tr>
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'order.transaction.id' | trans }}:</td>
-                <td style='padding:5px;'>{{ transactionID }}</td>
-            </tr>
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'payment_source' | trans }}:</td>
-                <td style='padding:5px;'>{{ lastFour }}</td>
+            <tr style="border: 1px solid #C8C8C8;">
+                <td style="padding:5px;">{{ "common.charged_at" | trans }}:</td>
+                <td style="padding:5px;">{{ orderTime }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'email.rent_amount' | trans }}:</td><td style="padding:5px;">{{ rentAmount }}</td>
+                <td style="padding:5px;">{{ "order.transaction.id" | trans }}:</td>
+                <td style="padding:5px;">{{ transactionID }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'email.other_amount' | trans }}:</td><td style="padding:5px;">{{ otherAmount }}</td>
+                <td style="padding:5px;">{{ "payment_source" | trans }}:</td>
+                <td style="padding:5px;">{{ lastFour }}</td>
+            </tr>
+            <tr style="border: 1px solid #C8C8C8;">
+              <td style="padding:5px;">{{ "email.rent_amount" | trans }}:</td><td style="padding:5px;">{{ rentAmount }}</td>
+            </tr>
+            <tr style="border: 1px solid #C8C8C8;">
+              <td style="padding:5px;">{{ "email.other_amount" | trans }}:</td><td style="padding:5px;">{{ otherAmount }}</td>
             </tr>
             {% if fee > 0 %}
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'order.fee' | trans }}:</td>
-                <td style='padding:5px;'>{{ fee }}</td>
+            <tr style="border: 1px solid #C8C8C8;">
+                <td style="padding:5px;">{{ "order.fee" | trans }}:</td>
+                <td style="padding:5px;">{{ fee }}</td>
             </tr>
             {% else %}
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'order.fee' | trans }}:</td>
-                <td style='padding:5px;'>{{ 'order.fee.free' | trans }}</td>
+            <tr style="border: 1px solid #C8C8C8;">
+                <td style="padding:5px;">{{ "order.fee" | trans }}:</td>
+                <td style="padding:5px;">{{ "order.fee.free" | trans }}</td>
             </tr>
             {% endif %}
-            <tr style='border: 1px solid #C8C8C8;'>
-                <td style='padding:5px;'>{{ 'order.total' | trans }}:</td>
-                <td style='padding:5px;'>{{ total }}</td>
+            <tr style="border: 1px solid #C8C8C8;">
+                <td style="padding:5px;">{{ "order.total" | trans }}:</td>
+                <td style="padding:5px;">{{ total }}</td>
             </tr>
 
             </tbody>
             </table>
             </br>
-        {% if type == 'bank' %}
+        {% if type == "bank" %}
         This payment will appear on your bank statement as RENTTRACK.
-        {% elseif type == 'card' %}
+        {% elseif type == "card" %}
         This payment will appear on your bank statement as {{ statementDescriptor }}.
         {% endif %}
         {% endblock %}
 tmplt;
         $this->addSql(
             "UPDATE email_translation
-            SET value = $template
+            SET value = '$template'
             WHERE (property = 'body' or property = 'bodyHtml')
             AND translatable_id = (SELECT id FROM email WHERE name = 'rjPendingOrder.html')"
         );
@@ -95,31 +95,31 @@ tmplt;
         >
           <tbody>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'common.charged_at' | trans }}:</td><td style="padding:5px;">{{ datetime }}</td>
+              <td style="padding:5px;">{{ "common.charged_at" | trans }}:</td><td style="padding:5px;">{{ datetime }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'order.transaction.id' | trans }}:</td><td style="padding:5px;">{{ transactionID }}</td>
+              <td style="padding:5px;">{{ "order.transaction.id" | trans }}:</td><td style="padding:5px;">{{ transactionID }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'payment_source' | trans }}:</td><td style="padding:5px;">{{ lastFour }}</td>
+              <td style="padding:5px;">{{ "payment_source" | trans }}:</td><td style="padding:5px;">{{ lastFour }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'email.rent_amount' | trans }}:</td><td style="padding:5px;">{{ rentAmount }}</td>
+              <td style="padding:5px;">{{ "email.rent_amount" | trans }}:</td><td style="padding:5px;">{{ rentAmount }}</td>
             </tr>
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'email.other_amount' | trans }}:</td><td style="padding:5px;">{{ otherAmount }}</td>
+              <td style="padding:5px;">{{ "email.other_amount" | trans }}:</td><td style="padding:5px;">{{ otherAmount }}</td>
             </tr>
             {% if fee > 0 %}
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'order.fee' | trans }}:</td><td style="padding:5px;">{{ fee }}</td>
+              <td style="padding:5px;">{{ "order.fee" | trans }}:</td><td style="padding:5px;">{{ fee }}</td>
             </tr>
             {% else %}
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'order.fee' | trans }}:</td><td style="padding:5px;">{{ 'order.fee.free' | trans }}</td>
+              <td style="padding:5px;">{{ "order.fee" | trans }}:</td><td style="padding:5px;">{{ "order.fee.free" | trans }}</td>
             </tr>
             {% endif %}
             <tr style="border: 1px solid #C8C8C8;">
-              <td style="padding:5px;">{{ 'order.total' | trans }}:</td><td style="padding:5px;">{{ total }}</td>
+              <td style="padding:5px;">{{ "order.total" | trans }}:</td><td style="padding:5px;">{{ total }}</td>
             </tr>
 
           </tbody>
@@ -127,9 +127,9 @@ tmplt;
         If you have any questions, please call 866.841.9090 or email help@renttrack.com.
         <br/>
         <br/>
-        {% if type == 'bank' %}
+        {% if type == "bank" %}
         This payment will appear on your bank statement as RENTTRACK.
-        {% elseif type == 'card' %}
+        {% elseif type == "card" %}
         This payment will appear on your bank statement as {{ statementDescriptor }}.
         {% endif %}
         <br/>
@@ -140,7 +140,7 @@ tmplt;
 tmplt;
         $this->addSql(
             "UPDATE email_translation
-            SET value = $template
+            SET value = '$template'
             WHERE (property = 'body' or property = 'bodyHtml')
             AND translatable_id = (SELECT id FROM email WHERE name = 'rjOrderReceipt.html')"
         );
