551c405b131d55a87f34a705e269df96fc009d22
Yuriy Borovchuk <yurez.b@gmail.com>
2016-02-15 21:01:16 +0200

    RT-2106: Only flag DTR payments over limit

--- src/RentJeeves/CheckoutBundle/Tests/Unit/DoD/Rule/PaymentAmountMaxLimitCase.php
+++ src/RentJeeves/CheckoutBundle/Tests/Unit/DoD/Rule/PaymentAmountMaxLimitCase.php
@@ -2,8 +2,11 @@
 
 namespace RentJeeves\CheckoutBundle\Tests\Unit\DoD\Rule;
 
+use CreditJeeves\DataBundle\Entity\Group;
 use RentJeeves\CheckoutBundle\DoD\Rule\PaymentAmountMaxLimit;
+use RentJeeves\DataBundle\Entity\Contract;
 use RentJeeves\DataBundle\Entity\Payment;
+use RentJeeves\DataBundle\Enum\OrderAlgorithmType;
 use RentJeeves\TestBundle\Tests\Unit\UnitTestBase;
 
 class PaymentAmountMaxLimitCase extends UnitTestBase
@@ -11,25 +14,67 @@ class PaymentAmountMaxLimitCase extends UnitTestBase
     /**
      * @test
      */
-    public function shouldReturnFalseIfPaymentAmountMoreThanLimit()
+    public function shouldReturnFalseIfPaymentAmountMoreThanLimitForGroupWithOrderAlgorithmTypePayDirect()
     {
+        $group = new Group();
+        $group->setOrderAlgorithm(OrderAlgorithmType::PAYDIRECT);
+
+        $contract = new Contract();
+        $contract->setGroup($group);
+
         $payment = new Payment();
+        $payment->setContract($contract);
         $payment->setAmount(501);
 
         $limitRule = new PaymentAmountMaxLimit(500);
-        $this->assertFalse($limitRule->checkPayment($payment), '501 exceeds 500, check should return false');
+        $this->assertFalse(
+            $limitRule->checkPayment($payment),
+            '501 exceeds 500 and group has order algorithm "pay_direct", check should return false'
+        );
     }
 
     /**
      * @test
      */
-    public function shouldReturnTrueIfPaymentAmountAllowedByLimit()
+    public function shouldReturnTrueIfPaymentAmountAllowedByLimitForGroupWithOrderAlgorithmTypePayDirect()
     {
+        $group = new Group();
+        $group->setOrderAlgorithm(OrderAlgorithmType::PAYDIRECT);
+
+        $contract = new Contract();
+        $contract->setGroup($group);
+
         $payment = new Payment();
+        $payment->setContract($contract);
         $payment->setAmount(500);
 
         $limitRule = new PaymentAmountMaxLimit(500);
-        $this->assertTrue($limitRule->checkPayment($payment), '500 doesn\'t exceed 500, should return true');
+        $this->assertTrue(
+            $limitRule->checkPayment($payment),
+            '500 doesn\'t exceed 500 and group has order algorithm "pay_direct", should return true'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnTrueIfPaymentAmountMoreThanLimitForGroupWithOrderAlgorithmTypeNotPayDirect()
+    {
+        $group = new Group();
+        $group->setOrderAlgorithm(OrderAlgorithmType::SUBMERCHANT);
+
+        $contract = new Contract();
+        $contract->setGroup($group);
+
+        $payment = new Payment();
+        $payment->setContract($contract);
+        $payment->setAmount(501);
+
+        $limitRule = new PaymentAmountMaxLimit(500);
+        $this->assertTrue(
+            $limitRule->checkPayment($payment),
+            '501 exceeds 500 and group has order algorithm no "pay_direct", check should return true'
+        );
     }
 
     /**
