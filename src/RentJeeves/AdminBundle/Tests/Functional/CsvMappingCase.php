3515f7363bdb9b2075f66fea10e8599fe649770f
Alexandr Sharamko <sharamko11@yandex.ru>
2016-03-02 12:22:11 +0200

    RT-2037 added test for admin csv mapping

--- src/RentJeeves/AdminBundle/Tests/Functional/CsvMappingCase.php
+++ src/RentJeeves/AdminBundle/Tests/Functional/CsvMappingCase.php
@@ -2,14 +2,15 @@
 
 namespace RentJeeves\AdminBundle\Tests\Functional;
 
-use RentJeeves\TestBundle\Functional\BaseTestCase;
+use RentJeeves\LandlordBundle\Tests\Functional\ImportBaseAbstract;
+use RentJeeves\LandlordBundle\Accounting\Import\Mapping\MappingAbstract as ImportMapping;
 
-class CsvMappingCase extends BaseTestCase
+class CsvMappingCase extends ImportBaseAbstract
 {
     /**
      * @test
      */
-    public function editContract()
+    public function map()
     {
         $this->load(true);
         $this->setDefaultSession('symfony');
@@ -21,5 +22,35 @@ class CsvMappingCase extends BaseTestCase
         $editLink->click();
         $tabLink = $this->getDomElement('.nav-tabs li>a:contains("Import Defaults")');
         $tabLink->click();
+
+        $this->assertNotEmpty($mappingLink = $this->page->find('css', '#createCsvMappingLink'), 'Should see link csv mapping');
+        $mappingLink->click();
+
+        $this->assertNotNull(
+            $attFile = $this->page->find('css', '#upload_csv_file_attachment'),
+            'Attach does not exist'
+        );
+        $filePath = $this->getFilePathByName('import.csv');
+        $attFile->attachFile($filePath);
+        $this->assertNotEmpty(
+            $buttonUpload = $this->page->find('css', '#upload_csv_file_upload'),
+            'Should see link csv mapping'
+        );
+        $buttonUpload->click();
+
+        $mapFile = $this->mapFile;
+        $mapFile[15] = ImportMapping::KEY_TENANT_STATUS;
+
+        $this->fillCsvMapping($mapFile, 15, '#match_file_type_column');
+        $this->assertNotEmpty(
+            $buttonSave = $this->page->find('css', '#match_file_type_save'),
+            'Should see link csv mapping'
+        );
+
+        $buttonSave->click();
+        $this->assertNotEmpty(
+            $buttonSave = $this->page->find('css', '.alert-success'),
+            'Should see link csv mapping'
+        );
     }
 }
