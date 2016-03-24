59402d28e275ba62873fe70bdeb057af0d572ea2
Alexandr Sharamko <sharamko11@yandex.ru>
2016-03-24 12:46:27 +0200

    RT-1627 setup credit-jeeves source for jms job bundle, removed doc comment for migration

--- app/DoctrineMigrations/Version20160322174040.php
+++ app/DoctrineMigrations/Version20160322174040.php
@@ -5,9 +5,6 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20160322174040 extends AbstractMigration
 {
     public function up(Schema $schema)
