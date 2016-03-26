a74637a4154923b52e417ffa59837c0d8fa6ec36
Cary Penniman <cary@renttrack.com>
2016-03-26 14:52:05 -0700

    RT-2289 : Added MailAuthorizer with WestSide Rentals email whitelist

---
+++ src/CreditJeeves/CoreBundle/Mailer/MailAuthorizer.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CreditJeeves\CoreBundle\Mailer;
+
+use CreditJeeves\DataBundle\Entity\User;
+use RentJeeves\DataBundle\Entity\Partner;
+
+class MailAuthorizer
+{
+    protected static $westsideWhitelist = [
+        "rjOrderCancel.html",
+        "rjOrderError.html",
+        "rjOrderReceipt.html",
+        "rjOrderRefunding.html",
+        "rjOrderReissued.html",
+        "rjOrderSending.html",
+        "rjPaymentDue.html",
+        "rjPendingOrder.html"
+    ];
+
+    public static function isAllowed($emailTemplateName, User $user)
+    {
+        if (self::blockedForUser($user)) {
+            return false;
+        } elseif (self::blockedForPartner($emailTemplateName, $user)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected static function blockedForUser(User $user)
+    {
+        if (false === $user->getEmailNotification()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected static function blockedForPartner($emailTemplateName, User $user)
+    {
+        /** @var Partner $partner */
+        if (false != $partner = $user->getPartner()) {
+            switch ($partner->getRequestName()) {
+                case 'WESTSIDE':
+                    return !in_array($emailTemplateName, self::$westsideWhitelist);
+                    break;
+            }
+        }
+
+        return false;
+    }
+}
