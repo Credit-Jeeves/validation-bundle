79c2f01763701bd8a1a7d73b56d337909ca7656f
onatskyy <onatskyy@gmail.com>
2016-03-29 16:24:25 +0300

    RT-2250: fixed codestyle

--- src/RentJeeves/DataBundle/Entity/ProfitStarsBatch.php
+++ src/RentJeeves/DataBundle/Entity/ProfitStarsBatch.php
@@ -4,6 +4,7 @@ namespace RentJeeves\DataBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use RentJeeves\DataBundle\Model\ProfitStarsBatch as Base;
+
 /**
  * @ORM\Table(name="rj_profitstars_batch")
  * @ORM\Entity
