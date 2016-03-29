c1daaba1c40a34ec57c1be7a0498820d0ea367a3
Yuriy Borovchuk <yurez.b@gmail.com>
2016-03-29 19:57:22 +0300

    RT-2109: Created migration for allowed ach and allowed credit card

--- src/CreditJeeves/DataBundle/Model/Group.php
+++ src/CreditJeeves/DataBundle/Model/Group.php
@@ -238,13 +238,6 @@ abstract class Group
     protected $created_at;
 
     /**
-     * @var boolean
-     *
-     * @ORM\Column(name="disable_credit_card", type="boolean", options={"default"="0"})
-     */
-    protected $disableCreditCard;
-
-    /**
      * @Gedmo\Timestampable(on="update")
      * @ORM\Column(type="datetime")
      */
@@ -492,7 +485,6 @@ abstract class Group
         $this->billingAccounts = new ArrayCollection();
         $this->waitingContracts = new ArrayCollection();
         $this->importSummaries = new ArrayCollection();
-        $this->disableCreditCard = false;
         $this->depositAccounts = new ArrayCollection();
     }
 
@@ -1449,22 +1441,6 @@ abstract class Group
     public function setStatementDescriptor($statementDescriptor)
     {
         $this->statementDescriptor = $statementDescriptor;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function isDisableCreditCard()
-    {
-        return $this->disableCreditCard;
-    }
-
-    /**
-     * @param mixed $disableCreditCard
-     */
-    public function setDisableCreditCard($disableCreditCard)
-    {
-        $this->disableCreditCard = $disableCreditCard;
     }
 
     /**
