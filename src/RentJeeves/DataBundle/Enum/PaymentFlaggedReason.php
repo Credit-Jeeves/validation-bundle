edd14d5a16d73566407e62c7d1e6f99c93e79e66
Alexandr Sharamko <sharamko11@yandex.ru>
2016-03-30 15:57:16 +0300

    RT-2266 added payment reason enum, migration, field

---
+++ src/RentJeeves/DataBundle/Enum/PaymentFlaggedReason.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RentJeeves\DataBundle\Enum;
+
+use CreditJeeves\CoreBundle\Enum;
+
+class PaymentFlaggedReason extends Enum
+{
+    const AMOUNT_LIMIT_EXCEEDED = 'amount_limit_exceeded';
+
+    const EXECUTION_DISALLOWED = 'execution_disallowed';
+
+    const DTR_FIRST_PAYMENT = 'dtr_first_payment';
+
+    const OUTSIDE_DTR_ROLLING_WINDOW = 'outside_dtr_rolling_window';
+
+    const DTR_MONTH_LIMIT_OVERFLOWED = 'dtr_month_limit_overflowed';
+
+    const DTR_UNTRUSTED_LANDLORD = 'dtr_untrusted_landlord';
+}
