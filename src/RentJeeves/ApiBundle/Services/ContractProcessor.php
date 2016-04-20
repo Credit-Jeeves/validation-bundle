d79b3e17e4b116400842b3b3e9ae3f34e8fda40b
Yuriy Borovchuk <yurez.b@gmail.com>
2016-04-20 22:23:46 +0300

    RT-2342: Added group settings and deposit accounts for creating groups from api; Added test for full create structure from api on create contract

--- src/RentJeeves/ApiBundle/Services/ContractProcessor.php
+++ src/RentJeeves/ApiBundle/Services/ContractProcessor.php
@@ -15,13 +15,17 @@ use RentJeeves\CoreBundle\Services\ContractProcess;
 use RentJeeves\CoreBundle\Services\Exception\PropertyManagerUnitOwnershipException;
 use RentJeeves\CoreBundle\Services\PropertyManager;
 use RentJeeves\DataBundle\Entity\Contract;
+use RentJeeves\DataBundle\Entity\DepositAccount;
+use RentJeeves\DataBundle\Entity\GroupSettings;
 use RentJeeves\DataBundle\Entity\Landlord;
 use RentJeeves\DataBundle\Entity\PropertyAddress;
 use RentJeeves\DataBundle\Entity\Tenant;
 use RentJeeves\DataBundle\Entity\Unit;
 use RentJeeves\DataBundle\Enum\ContractStatus;
+use RentJeeves\DataBundle\Enum\DepositAccountStatus;
+use RentJeeves\DataBundle\Enum\DepositAccountType;
 use RentJeeves\DataBundle\Enum\OrderAlgorithmType;
-use RentJeeves\DataBundle\Enum\TrustedLandlordType;
+use RentJeeves\DataBundle\Enum\PaymentProcessor;
 use RentJeeves\TrustedLandlordBundle\Exception\TrustedLandlordServiceException;
 use RentJeeves\TrustedLandlordBundle\Model\TrustedLandlordDTO;
 use RentJeeves\TrustedLandlordBundle\Services\TrustedLandlordService;
@@ -34,6 +38,8 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
  */
 class ContractProcessor
 {
+    const DEFAULT_STATEMENT_DESCRIPTOR = 'RENTTRK-WSR RENT PAY';
+
     /**
      * @var EntityManager
      */
@@ -70,6 +76,24 @@ class ContractProcessor
     protected $trustedLandlordService;
 
     /**
+     * @see parameter paydirect_fee_cc
+     * @var float
+     */
+    protected $defaultFeeCC;
+
+    /**
+     * @see parameter paydirect_fee_ach
+     * @var float
+     */
+    protected $defaultFeeACH;
+
+    /**
+     * @see parameter aci.collect_pay.pay_direct_escrow_account
+     * @var string
+     */
+    protected $defaultPayDirectInboundMerchantAccount;
+
+    /**
      * @param EntityManager $em
      * @param LoggerInterface $logger
      * @param Mailer $mailer
@@ -77,6 +101,9 @@ class ContractProcessor
      * @param ContractProcess $contractProcess
      * @param PropertyManager $propertyProcess
      * @param TrustedLandlordService $trustedLandlordService
+     * @param $defaultFeeCC
+     * @param $defaultFeeACH
+     * @param $defaultPayDirectInboundMerchantAccount
      *
      * @DI\InjectParams({
      *     "em" = @DI\Inject("doctrine.orm.default_entity_manager"),
@@ -85,7 +112,10 @@ class ContractProcessor
      *     "locale" = @DI\Inject("%kernel.default_locale%"),
      *     "contractProcess" = @DI\Inject("contract.process"),
      *     "propertyProcess" = @DI\Inject("property.manager"),
-     *     "trustedLandlordService" = @DI\Inject("trusted_landlord_service")
+     *     "trustedLandlordService" = @DI\Inject("trusted_landlord_service"),
+     *     "defaultFeeCC" = @DI\Inject("%paydirect_fee_cc%"),
+     *     "defaultFeeACH" = @DI\Inject("%paydirect_fee_ach%"),
+     *     "defaultPayDirectInboundMerchantAccount" = @DI\Inject("%aci.collect_pay.pay_direct_escrow_account%")
      * })
      */
     public function __construct(
@@ -95,7 +125,10 @@ class ContractProcessor
         $locale,
         ContractProcess $contractProcess,
         PropertyManager $propertyProcess,
-        TrustedLandlordService $trustedLandlordService
+        TrustedLandlordService $trustedLandlordService,
+        $defaultFeeCC,
+        $defaultFeeACH,
+        $defaultPayDirectInboundMerchantAccount
     ) {
         $this->em = $em;
         $this->logger = $logger;
@@ -104,6 +137,9 @@ class ContractProcessor
         $this->contractProcess = $contractProcess;
         $this->propertyManager = $propertyProcess;
         $this->trustedLandlordService = $trustedLandlordService;
+        $this->defaultFeeCC = $defaultFeeCC;
+        $this->defaultFeeACH = $defaultFeeACH;
+        $this->defaultPayDirectInboundMerchantAccount = $defaultPayDirectInboundMerchantAccount;
     }
 
     /**
@@ -199,34 +235,15 @@ class ContractProcessor
         }
 
         if (!$group = $trustedLandlord->getGroup()) {
-            $holding = new Holding();
-            $holding->setName($this->generateCommercialName($trustedLandlordDTO));
-
-            $group = new Group();
-            $group->setName($this->generateCommercialName($trustedLandlordDTO));
-            $group->setType(GroupType::RENT);
-            $group->setOrderAlgorithm(OrderAlgorithmType::PAYDIRECT);
-            $group->setHolding($holding);
+            $group = $this->createGroup($trustedLandlordDTO);
             $group->setTrustedLandlord($trustedLandlord);
-
-            $holding->addGroup($group);
-
-            $this->em->persist($holding);
         }
 
         if ($email = $trustedLandlordDTO->getEmail() and
             !$this->em->getRepository('DataBundle:User')->findOneByEmail($email)
         ) {
-            $landlord = new Landlord();
-            $landlord->setEmail($email);
-            $landlord->setFirstName($trustedLandlordDTO->getFirstName());
-            $landlord->setLastName($trustedLandlordDTO->getLastName());
-            $landlord->setPhone($trustedLandlordDTO->getPhone());
-            $landlord->setPassword(md5(md5(rand())));
-            $landlord->setCulture($this->locale);
-            $landlord->setHolding($group->getHolding());
-            $landlord->setAgentGroups($group);
 
+            $landlord = $this->createLandlord($trustedLandlordDTO, $group);
             $this->em->persist($landlord);
         }
 
@@ -281,6 +298,95 @@ class ContractProcessor
         }
 
         throw new \LogicException('Contract should be created');
+    }
+
+    /**
+     * @param TrustedLandlordDTO $trustedLandlordDTO
+     * @return Group
+     */
+    protected function createGroup(TrustedLandlordDTO $trustedLandlordDTO)
+    {
+        $newGroup = new Group();
+
+        $newGroup->setName($this->generateCommercialName($trustedLandlordDTO));
+        $newGroup->setHolding($this->createHolding($newGroup->getName()));
+        $newGroup->setType(GroupType::RENT);
+        $newGroup->setStatementDescriptor(self::DEFAULT_STATEMENT_DESCRIPTOR);
+        $newGroup->setOrderAlgorithm(OrderAlgorithmType::PAYDIRECT);
+
+        $this->createGroupSetting($newGroup);
+        $this->createDepositAccount($newGroup);
+
+        return $newGroup;
+    }
+
+    /**
+     * @param string $holdingName
+     * @return Holding
+     */
+    protected function createHolding($holdingName)
+    {
+        $newHolding = new Holding();
+        $newHolding->setName($holdingName);
+
+        return $newHolding;
+    }
+
+    /**
+     * @param Group $group
+     *
+     * @return GroupSettings
+     */
+    protected function createGroupSetting(Group $group)
+    {
+        $newGroupSettings = $group->getGroupSettings();
+        $newGroupSettings->setPaymentProcessor(PaymentProcessor::ACI);
+        $newGroupSettings->setAutoApproveContracts(true);
+        $newGroupSettings->setPassedAch(true);
+        $newGroupSettings->setFeeCC($this->defaultFeeCC);
+        $newGroupSettings->setFeeACH($this->defaultFeeACH);
+
+        return $newGroupSettings;
+    }
+
+    /**
+     * @param Group $group
+     *
+     * @return DepositAccount
+     */
+    protected function createDepositAccount(Group $group)
+    {
+        $newDepositAccount = new DepositAccount($group);
+        $newDepositAccount->setType(DepositAccountType::RENT);
+        $newDepositAccount->setMerchantName($this->defaultPayDirectInboundMerchantAccount);
+        if ($this->defaultPayDirectInboundMerchantAccount) {
+            $newDepositAccount->setStatus(DepositAccountStatus::DA_COMPLETE);
+        }
+        $newDepositAccount->setPaymentProcessor(PaymentProcessor::ACI);
+
+        $group->addDepositAccount($newDepositAccount);
+
+        return $newDepositAccount;
+    }
+
+    /**
+     * @param TrustedLandlordDTO $trustedLandlordDTO
+     * @param Group $group
+     * @return Landlord
+     */
+    protected function createLandlord(TrustedLandlordDTO $trustedLandlordDTO, Group $group)
+    {
+        $landlord = new Landlord();
+        $landlord->setEmail($trustedLandlordDTO->getEmail());
+        $landlord->setFirstName($trustedLandlordDTO->getFirstName());
+        $landlord->setLastName($trustedLandlordDTO->getLastName());
+        $landlord->setPhone($trustedLandlordDTO->getPhone());
+        $landlord->setPassword(md5(md5(rand())));
+        $landlord->setCulture($this->locale);
+        $landlord->setHolding($group->getHolding());
+        $landlord->setAgentGroups($group);
+
+        return $landlord;
     }
 
     /**
