a86327f00c08596af515cfc54aefcb8ebc6611f7
Tatyana <shtatalex@gmail.com>
2016-04-21 14:05:03 +0300

    Fixed error in AccountingController

--- src/RentJeeves/LandlordBundle/Controller/AccountingController.php
+++ src/RentJeeves/LandlordBundle/Controller/AccountingController.php
@@ -148,7 +148,7 @@ class AccountingController extends Controller
     {
         /** @var ImportSettingsValidator $importSettingsValidator */
         $importSettingsValidator = $this->get('import.settings.validator');
-        if ($importSettingsValidator->isValidImportSettings($this->getUser()->getCurrentGroup()) === false) {
+        if ($importSettingsValidator->isValidImportSettings($this->getCurrentGroup()) === false) {
             return $this->render(
                 'LandlordBundle:Accounting:import_error.html.twig',
                 ['message' => $importSettingsValidator->getErrorMessage()]
