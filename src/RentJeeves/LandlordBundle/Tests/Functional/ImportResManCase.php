7206fed6eccbb15e6af8136ecbc99e62ceb30d02
Alexandr Sharamko <sharamko11@yandex.ru>
2016-05-04 15:02:40 +0300

    RT-1080 added possibility for resman: only review and post

--- src/RentJeeves/LandlordBundle/Tests/Functional/ImportResManCase.php
+++ src/RentJeeves/LandlordBundle/Tests/Functional/ImportResManCase.php
@@ -47,7 +47,7 @@ class ImportResManCase extends ImportBaseAbstract
 
         // We must make sure the data saved into DB, so we count before import and after
         $contract = $em->getRepository('RjDataBundle:Contract')->findAll();
-        $this->assertCount(24, $contract, 'Check fixtures, should be present just 23 contracts on DB');
+        $this->assertCount(24, $contract, 'Check fixtures, should be present just 24 contracts on DB');
 
         $this->setDefaultSession('selenium2');
         $this->login('landlord1@example.com', 'pass');
@@ -60,12 +60,69 @@ class ImportResManCase extends ImportBaseAbstract
             80000,
             "$('table').is(':visible')"
         );
-        //First page
+        //1
         $submitImport->click();
         $this->waitReviewAndPost();
-        //Second page
+        //2
         $submitImport->click();
         $this->waitReviewAndPost();
+        //3
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //4
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //5
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //6
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //7
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //8
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //9
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //10
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //11
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //12
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //13
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //14
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //15
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //16
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //17
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //18
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //19
+        $submitImport->click();
+        $this->waitReviewAndPost();
+        //20
+        $submitImport->click();
+        $this->waitReviewAndPost();
+
+        $this->waitRedirectToSummaryPage();
+        $this->assertNotNull($publicId = $this->page->find('css', '#publicId'));
 
         // We must make sure the data saved into DB, so we count before import and after
         $contracts = $em->getRepository('RjDataBundle:Contract')->findAll();
@@ -119,5 +176,31 @@ class ImportResManCase extends ImportBaseAbstract
             $this->page->find('css', '.rentNotEditable'),
             'We should not see not editable fields'
         );
+    }
+
+    /**
+     * @test
+     * @depends checkByResManRecurringChargeImport
+     */
+    public function checkOnlyReviewAndPostImport()
+    {
+        $this->setDefaultSession('selenium2');
+        $this->login('landlord1@example.com', 'pass');
+        $this->page->clickLink('tab.accounting');
+        //First Step
+        $this->session->wait(5000, "typeof jQuery != 'undefined'");
+        $this->assertNotNull($exceptionOnly = $this->page->find('css', '#import_file_type_onlyException'));
+        $exceptionOnly->check();
+        $submitImport = $this->getDomElement('.submitImportFile', 'Submit button should exist');
+        $submitImport->click();
+        $this->waitReview();
+        $this->session->wait(20000, "$('.submitImportFile>span').is(':visible')");
+        $trs = $this->getParsedTrsByStatus();
+        $this->assertCount(1, $trs, "Count statuses is wrong");
+        $this->assertCount(2, $trs['import.status.error'], "Count contract with status 'error' wrong");
+        $submitImport = $this->getDomElement('.submitImportFile', 'Submit button should exist');
+        $submitImport->click();
+        $this->waitRedirectToSummaryPage();
+        $this->assertNotNull($publicId = $this->page->find('css', '#publicId'));
     }
 }
