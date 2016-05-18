e550f74adf18f13973aedbbc125feab97663d3b1
Artem Panasenko <artemipanasenko@gmail.com>
2016-05-18 11:55:45 +0300

    RT-2420 Resolve merge conflict

--- src/RentJeeves/TestBundle/ProfitStars/ClientBuilder.php
+++ src/RentJeeves/TestBundle/ProfitStars/ClientBuilder.php
@@ -8,6 +8,8 @@ use RentTrack\ProfitStarsClientBundle\Client\ClientBuilder as Base;
 class ClientBuilder extends Base
 {
     /**
+     * @param LoggerInterface $logger
+     * @param bool $isDebug
      * @return TransactionReportingClient
      */
     public static function buildTransactionReportingClient(LoggerInterface $logger, $isDebug = false)
@@ -16,6 +18,8 @@ class ClientBuilder extends Base
     }
 
     /**
+     * @param LoggerInterface $logger
+     * @param bool $isDebug
      * @return RemoteDepositReportingClient
      */
     public static function buildRemoteDepositReportingClient(LoggerInterface $logger, $isDebug = false)
