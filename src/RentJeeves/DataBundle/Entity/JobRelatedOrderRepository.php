5d097a01a7b287363d1fb2c497e225655cad3cd3
Artem Panasenko <artemipanasenko@gmail.com>
2016-05-31 14:36:53 +0300

    RT-2505 Create function for retry failed jobs when changed LeaseId, add test for Resman system

--- src/RentJeeves/DataBundle/Entity/JobRelatedOrderRepository.php
+++ src/RentJeeves/DataBundle/Entity/JobRelatedOrderRepository.php
@@ -11,7 +11,7 @@ class JobRelatedOrderRepository extends EntityRepository
      * @param \DateTime $date
      * @return JobRelatedOrder[]
      */
-    public function getFailedPushJobsToExternalApi(array $groups, \DateTime $date)
+    public function getFailedPaymentPushJobsByGroups(array $groups, \DateTime $date)
     {
         if (empty($groups)) {
             return [];
@@ -28,6 +28,30 @@ class JobRelatedOrderRepository extends EntityRepository
             ->andWhere('job.state = :failed')
             ->andWhere('DATE(job_related.createdAt) = :date')
             ->setParameter('groupId', $groups)
+            ->setParameter('failed', Job::STATE_FAILED)
+            ->setParameter('command', 'external_api:payment:push')
+            ->setParameter('date', $date->format('Y-m-d'))
+            ->getQuery()
+            ->execute();
+    }
+
+    /**
+     * @param Contract $contract
+     * @param \DateTime $date
+     * @return mixed
+     */
+    public function getFailedPaymentPushJobsByContract(Contract $contract, \DateTime $date)
+    {
+        return $this->createQueryBuilder('job_related')
+            ->innerJoin('job_related.job', 'job')
+            ->innerJoin('job_related.order', 'order')
+            ->innerJoin('order.operations', 'operations')
+            ->innerJoin('operations.contract', 'contract')
+            ->andWhere('contract.id = :contractId')
+            ->andWhere('job.command = :command')
+            ->andWhere('job.state = :failed')
+            ->andWhere('DATE(job_related.createdAt) = :date')
+            ->setParameter('contractId', $contract->getId())
             ->setParameter('failed', Job::STATE_FAILED)
             ->setParameter('command', 'external_api:payment:push')
             ->setParameter('date', $date->format('Y-m-d'))
--- src/RentJeeves/ExternalApiBundle/Tests/Services/ResMan/ResManContractSynchronizerCase.php
+++ src/RentJeeves/ExternalApiBundle/Tests/Services/ResMan/ResManContractSynchronizerCase.php
@@ -2,7 +2,10 @@
 
 namespace RentJeeves\ExternalApiBundle\Tests\Services\ResMan;
 
+use CreditJeeves\DataBundle\Entity\Operation;
 use RentJeeves\DataBundle\Entity\Contract;
+use RentJeeves\DataBundle\Entity\Job;
+use RentJeeves\DataBundle\Entity\JobRelatedOrder;
 use RentJeeves\DataBundle\Entity\UnitMapping;
 use RentJeeves\DataBundle\Enum\AccountingSystem;
 use RentJeeves\ExternalApiBundle\Tests\Services\ContractSynchronizerTestBase as Base;
@@ -95,6 +98,70 @@ class ResManContractSynchronizerCase extends Base
 
         $contract = $this->getEntityManager()->find('RjDataBundle:Contract', 20);
         $this->assertNotEquals(123321, $contract->getRent(), 'Rent should be updated');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRetryFailedJobsWhenContractsGetsExternalLeaseId()
+    {
+        $this->load(true);
+        /** @var Contract $contract */
+        $contract = $this->getEntityManager()->find('RjDataBundle:Contract', 20);
+
+        $contract->getHolding()->setAccountingSystem(AccountingSystem::RESMAN);
+        $contract->getHolding()->setUseRecurringCharges(true);
+        $contract->getUnit()->setName('test_resman_external_property_id|1|1101');
+
+        $contract->setExternalLeaseId(null);
+        $contract->getGroup()->getGroupSettings()->setIsIntegrated(true);
+        $propertyMapping = $contract->getProperty()->getPropertyMappingByHolding($contract->getHolding());
+        $propertyMapping->setExternalPropertyId('test_resman_external_property_id');
+
+        $residentMapping = $contract->getTenant()->getResidentForHolding($contract->getHolding());
+        $residentMapping->setResidentId('0529ec65-1310-47aa-8570-a902b927f92c');
+
+        $unitMapping = new UnitMapping();
+        $unitMapping->setExternalUnitId('test_resman_external_property_id|1|1101');
+        $unitMapping->setUnit($contract->getUnit());
+        $contract->getUnit()->setUnitMapping($unitMapping);
+        $this->getEntityManager()->persist($unitMapping);
+
+        /** @var Operation $operation */
+        $this->assertNotEmpty($operation = $contract->getOperations()->first(), 'Contract should has operations');
+        $order = $operation->getOrder();
+        $jobRelatedToOrder = new JobRelatedOrder();
+        $jobRelatedToOrder->setOrder($order);
+        $jobRelatedToOrder->setCreatedAt(new \DateTime());
+        $failedJob = new Job('external_api:payment:push');
+        $failedJob->addRelatedEntity($jobRelatedToOrder);
+        $failedJob->setState(Job::STATE_FAILED, $force = true);
+        $this->getEntityManager()->persist($failedJob);
+        $this->getEntityManager()->persist($jobRelatedToOrder);
+
+        $this->getEntityManager()->flush();
+
+        $this->getResManContractSynchronizer()->syncBalanceForHoldingAndExternalPropertyId(
+            $contract->getHolding(),
+            'test_resman_external_property_id'
+        );
+
+        $jobs = $this->getEntityManager()->getRepository('RjDataBundle:Job')->findAll();
+        $this->assertNotEmpty($jobs, 'Should be find at least one job');
+        $lastJob = end($jobs);
+
+        $this->assertEquals($failedJob->getId(), $lastJob->getOriginalJob()->getId(), 'JobIds should be equals');
+        $this->assertEquals('pending', $lastJob->getState(), 'Job should be pending');
+        $this->assertEquals(
+            'external_api:payment:push',
+            $lastJob->getCommand(),
+            'Expected: New job is for external_api:payment:push'
+        );
+        $this->assertEquals(
+            $order->getId(),
+            $lastJob->getRelatedEntities()->first()->getOrder()->getId(),
+            'Order\'s IDs should be equal'
+        );
     }
 
     /**
