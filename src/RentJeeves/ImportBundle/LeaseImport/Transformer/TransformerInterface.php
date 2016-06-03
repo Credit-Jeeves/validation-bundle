8a7da547c1ee2c7127d8e176b4939489b7ca8db5
onatskyy <onatskyy@gmail.com>
2016-06-03 14:25:29 +0300

    RT-2379: added transformer and tests

---
+++ src/RentJeeves/ImportBundle/LeaseImport/Transformer/TransformerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RentJeeves\ImportBundle\LeaseImport\Transformer;
+
+use RentJeeves\DataBundle\Entity\Import;
+use RentJeeves\ImportBundle\Exception\ImportTransformerException;
+
+interface TransformerInterface
+{
+    /**
+     * Transform the accounting system specific data models into generic import records (ImportLease)
+     *
+     * @param array $accountingSystemData accounting system specific model objects
+     * @param Import $import new ImportLeases will be add to this Object
+     *
+     * @throws ImportTransformerException if can`t transform data
+     */
+    public function transformData(array $accountingSystemData, Import $import);
+}
---
+++ src/RentJeeves/ImportBundle/Tests/Unit/LeaseImport/Transformer/TransformerFactoryCase.php
@@ -0,0 +1,413 @@
+<?php
+
+namespace RentJeeves\ImportBundle\Tests\Unit\LeaseImport\Transformer;
+
+use CreditJeeves\DataBundle\Entity\Group;
+use CreditJeeves\DataBundle\Entity\Holding;
+use RentJeeves\DataBundle\Entity\ImportGroupSettings;
+use RentJeeves\DataBundle\Enum\AccountingSystem;
+use RentJeeves\DataBundle\Enum\ImportModelType;
+use RentJeeves\DataBundle\Enum\ImportSource;
+use RentJeeves\ImportBundle\LeaseImport\Transformer\CsvTransformer;
+use RentJeeves\ImportBundle\LeaseImport\Transformer\MRITransformer;
+use RentJeeves\ImportBundle\LeaseImport\Transformer\TransformerFactory;
+use RentJeeves\ImportBundle\LeaseImport\Transformer\TransformerInterface;
+use RentJeeves\TestBundle\Tests\Unit\UnitTestBase;
+use RentJeeves\TestBundle\Traits\CreateSystemMocksExtensionTrait;
+
+class TransformerFactoryCase extends UnitTestBase
+{
+    use CreateSystemMocksExtensionTrait;
+
+    /**
+     * @var array
+     */
+    protected $tmpFiles = [];
+
+    protected function createCustomTransformerFile()
+    {
+        $customFileDist = __DIR__ . '/../../../../LeaseImport/Transformer/Custom/ExampleCustomTransformer.php.dist';
+        $this->tmpFiles[] = __DIR__ . '/../../../../LeaseImport/Transformer/Custom/ExampleCustomTransformer.php';
+        copy($customFileDist, $this->tmpFiles[0]);
+    }
+
+    protected function createCustomAndParentTransformersFile()
+    {
+        $customFileDist = __DIR__ . '/../../../../LeaseImport/Transformer/Custom/ExampleCustomTransformer.php.dist';
+        $this->tmpFiles[] = __DIR__ . '/../../../../LeaseImport/Transformer/Custom/ExampleCustomTransformer.php';
+        copy($customFileDist, $this->tmpFiles[0]);
+        $customFileDist = __DIR__ . '/../../../Fixtures/CustomLeaseTransformers/ChildExampleCustomTransformer.php.dist';
+        $this->tmpFiles[] = __DIR__ . '/../../../Fixtures/CustomLeaseTransformers/ChildExampleCustomTransformer.php';
+        copy($customFileDist, $this->tmpFiles[1]);
+    }
+
+    protected function createInvalidCustomTransformerFile()
+    {
+        $customFileDist = __DIR__ . '/../../../Fixtures/CustomLeaseTransformers/InvalidCustomTransformer.php.dist';
+        $this->tmpFiles[] = __DIR__ . '/../../../Fixtures/CustomLeaseTransformers/InvalidCustomTransformer.php';
+        copy($customFileDist, $this->tmpFiles[0]);
+    }
+
+    protected function createCustomTransformerWithInvalidNamespace()
+    {
+        $customFileDist = __DIR__ .
+            '/../../../Fixtures/CustomLeaseTransformers/CustomTransformerWithInvalidNamespace.php.dist';
+        $this->tmpFiles[] = __DIR__ .
+            '/../../../Fixtures/CustomLeaseTransformers/CustomTransformerWithInvalidNamespace.php';
+        copy($customFileDist, $this->tmpFiles[0]);
+    }
+
+    public function tearDown()
+    {
+        foreach ($this->tmpFiles as $tmpFile) {
+            if (true === file_exists($tmpFile)) {
+                unlink($tmpFile);
+            }
+        }
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     * @expectedException \RentJeeves\ImportBundle\Exception\ImportException
+     * @expectedExceptionMessage Not found any files with name "ExampleCustomTransformer.php"
+     */
+    public function shouldThrowExceptionIfDbHasRecordForInputDataButFileWithReturnedNameNotFound()
+    {
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue('ExampleCustomTransformer'));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../../LeaseImport/Transformer/Custom']
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $this->getMriTransformerMock());
+        $factory->getTransformer($group, 'test');
+    }
+
+    /**
+     * @test
+     * @expectedException \RentJeeves\ImportBundle\Exception\ImportException
+     * @expectedExceptionMessage Custom transformer for this Group must be override
+     */
+    public function shouldThrowExceptionIfDbHasRecordForInputDataButClassNotOverrideBaseTransformer()
+    {
+        $this->createInvalidCustomTransformerFile();
+
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue('InvalidCustomTransformer'));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../Fixtures/CustomLeaseTransformers']
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $this->getMriTransformerMock());
+        $factory->getTransformer($group, 'test');
+    }
+
+    /**
+     * @test
+     * @expectedException \RentJeeves\ImportBundle\Exception\ImportException
+     * @expectedExceptionMessage Pls check name and namespace in custom file
+     */
+    public function shouldThrowExceptionIfDbHasRecordForInputDataButClassHaveIncorrectNamespace()
+    {
+        $this->createCustomTransformerWithInvalidNamespace();
+
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue('CustomTransformerWithInvalidNamespace'));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../Fixtures/CustomLeaseTransformers']
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $this->getMriTransformerMock());
+        $factory->getTransformer($group, 'test');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateCustomTransformerIfDbHasRecordForInputData()
+    {
+        $this->createCustomTransformerFile();
+
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue('ExampleCustomTransformer'));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+        $mriTransformer = new MRITransformer($this->getEntityManagerMock(), $this->getLoggerMock());
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../../LeaseImport/Transformer/Custom']
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $mriTransformer);
+        $transformer = $factory->getTransformer($group, 'test');
+
+        $this->assertInstanceOf(
+            TransformerInterface::class,
+            $transformer
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateCustomTransformerIfDbHasRecordAndCustomTransformerOverridesNotBaseTransformer()
+    {
+        $this->createCustomAndParentTransformersFile();
+
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue('ChildExampleCustomTransformer'));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+
+        $mriTransformer = new MRITransformer($this->getEntityManagerMock(), $this->getLoggerMock());
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [
+                __DIR__ . '/../../../../LeaseImport/Transformer/Custom',
+                __DIR__ . '/../../../Fixtures/CustomLeaseTransformers',
+            ]
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $mriTransformer);
+
+        $transformer = $factory->getTransformer($group, 'test');
+
+        $this->assertInstanceOf(TransformerInterface::class, $transformer);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnMRITransformerIfDbDoesNotHaveRecordForInputData()
+    {
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::MRI);
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue(null));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+        $mriTransformer = new MRITransformer($this->getEntityManagerMock(), $this->getLoggerMock());
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../../LeaseImport/Transformer/Custom'],
+            [AccountingSystem::MRI => $this->getMriTransformerMock()],
+            $this->geCsvTransformerMock()
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $mriTransformer);
+        $transformer = $factory->getTransformer($group, 'test');
+
+        $this->assertInstanceOf(
+            MRITransformer::class,
+            $transformer
+        );
+    }
+
+    /**
+     * @test
+     * @expectedException \RentJeeves\ImportBundle\Exception\ImportException
+     * @expectedExceptionMessage Accounting System with name "none" is not supported.
+     */
+    public function shouldThrowExceptionIfDbDoesNotHaveRecordForInputDataAndHoldingHaveIncorrectApiType()
+    {
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::INTEGRATED_API);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem('none');
+        $group->setHolding($holding);
+
+        $repoMock = $this->getImportTransformerRepositoryMock();
+        $repoMock->expects($this->once())
+            ->method('findClassNameWithPriorityByGroupAndExternalPropertyId')
+            ->with(
+                $this->equalTo($group),
+                $this->equalTo('test'),
+                $this->equalTo(ImportModelType::LEASE)
+            )
+            ->will($this->returnValue(null));
+
+        $em = $this->getEntityManagerMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+
+        $factory = new TransformerFactory(
+            $em,
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../../LeaseImport/Transformer/Custom']
+        );
+        $factory->addApiTransformer(AccountingSystem::MRI, $this->getMriTransformerMock());
+        $factory->getTransformer($group, 'test');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnCsvTransformerIfGroupSettingsHaveCsvSource()
+    {
+        $group = new Group();
+        $groupImportSettings = new ImportGroupSettings();
+        $groupImportSettings->setSource(ImportSource::CSV);
+        $group->setImportSettings($groupImportSettings);
+        $holding = new Holding();
+        $holding->setAccountingSystem(AccountingSystem::PROMAS);
+        $group->setHolding($holding);
+
+        $factory = new TransformerFactory(
+            $this->getEntityManagerMock(),
+            $this->getLoggerMock(),
+            [__DIR__ . '/../../../../LeaseImport/Transformer/Custom']
+        );
+
+        $factory->setCsvTransformer($this->getBaseMock(CsvTransformer::class));
+
+        $transformer = $factory->getTransformer($group, 'test');
+        $this->assertInstanceOf(CsvTransformer::class, $transformer);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\RentJeeves\DataBundle\Entity\ImportTransformerRepository
+     */
+    protected function getImportTransformerRepositoryMock()
+    {
+        return $this->getBaseMock('\RentJeeves\DataBundle\Entity\ImportTransformerRepository');
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|MRITransformer
+     */
+    protected function getMriTransformerMock()
+    {
+        return $this->getBaseMock(MRITransformer::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|CsvTransformer
+     */
+    protected function geCsvTransformerMock()
+    {
+        return $this->getBaseMock(CsvTransformer::class);
+    }
+}
