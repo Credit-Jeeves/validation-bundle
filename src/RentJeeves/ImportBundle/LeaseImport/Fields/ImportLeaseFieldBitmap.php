1bdbdfaa2f4ee63b8859df26082f7aceff226101
Alexandr Sharamko <sharamko11@yandex.ru>
2016-06-09 15:23:02 +0300

    RT-5448 developed LeaseBitmap

---
+++ src/RentJeeves/ImportBundle/LeaseImport/Fields/ImportLeaseFieldBitmap.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace RentJeeves\ImportBundle\LeaseImport\Fields;
+
+use RentJeeves\CoreBundle\Bitmap\Bitmap;
+
+/**
+ * This code should help us to understand what field we want to update in Lease
+ * We mark by bitNumber fields in this class
+ */
+class ImportLeaseFieldBitmap
+{
+    /**
+     * bitmask of fields that can be updated
+     *
+     * @var int
+     */
+    protected $updateMask;
+
+    /**
+     * bitmap of fields that differ
+     *
+     * @var Bitmap
+     */
+    protected $diffBitmap;
+
+    /**
+     * @param bool|false $isNew
+     */
+    public function __construct($isNew = false)
+    {
+        $this->updateMask = ($isNew) ? ImportLeaseFields::UPDATE_MASK_NEW : ImportLeaseFields::UPDATE_MASK_MATCHED;
+        $this->diffBitmap = new Bitmap(0); // no fields different
+    }
+
+    /**
+     * set as different
+     *
+     * @param int $bitNumber
+     */
+    public function markDifferent($bitNumber)
+    {
+        $this->diffBitmap->setBit($bitNumber);
+    }
+
+    /**
+     * check if different
+     *
+     * @param int $bitNumber
+     *
+     * @return bool
+     */
+    public function isDifferent($bitNumber)
+    {
+        return $this->diffBitmap->isBitSet($bitNumber);
+    }
+
+    /**
+     * check if we need to update this field
+     *
+     * @param int $bitNumber
+     * @return bool
+     */
+    public function isNeedUpdate($bitNumber)
+    {
+        return $this->diffBitmap->isBitSetWithinMask($bitNumber, $this->updateMask);
+    }
+
+    /**
+     * @return int
+     */
+    public function getUpdateMask()
+    {
+        return $this->updateMask;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDiffBitmap()
+    {
+        return $this->diffBitmap->getInt();
+    }
+}
---
+++ src/RentJeeves/ImportBundle/LeaseImport/Fields/ImportLeaseFields.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RentJeeves\ImportBundle\LeaseImport\Fields;
+
+class ImportLeaseFields
+{
+    const FIRST = 0;
+
+    const BALANCE = 0;
+
+    const RENT = 1;
+
+    const DUE_DATE = 2;
+
+    const FINISH_AT = 3;
+
+    const START_AT = 4;
+
+    const LAST = 4;
+
+    //Hexadecimal
+    const UPDATE_MASK_MATCHED = 0xb; // rent, balance, finishAt
+
+    const UPDATE_MASK_NEW = 0x1f; // all
+}
+
